0a30854081b19c8be69e4e837843f0400d3d1e40
Andreu Font-Ribera <font.andreu@gmail.com>
2017-11-24 15:40:43 +0000

    added example for issue with declications in colore

--- example_scripts/healpix_quasars.py
+++ example_scripts/healpix_quasars.py
@@ -4,14 +4,19 @@ import matplotlib.pyplot as plt
 import healpy as hp
 
 # identify output file we want to plot
-hdulist = fits.open('../Sims/NewLya512/skewers_512_srcs_s0_0.fits')
+hdulist = fits.open('../example_data/raw_colore/test_N1000.fits')
 
-# use only a fraction of the catalog
-Nq = 100000
-catalog = hdulist[1].data[0:Nq]
+# get quasar catalog
+initial_catalog = hdulist[1].data
+
+# some quasars have Dec=NaN, let's get rid of them for now
+mask = np.invert(np.isnan(initial_catalog['DEC']))
+catalog = initial_catalog[mask]
+print('initial size',len(initial_catalog))
+print('final size',len(catalog))
 
 # specify Nside parameter in HEALPix maps (power of 2)
-Nside=2**1
+Nside=2**2
 
 # convert (RA,DEC) to (theta,phi) and find HEALPix pixel for each point
 pix = hp.ang2pix(Nside,(catalog['DEC']+90.0)/180.0*np.pi,catalog['RA']/180.0*np.pi)
