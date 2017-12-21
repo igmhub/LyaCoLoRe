f05d4f2e7306e0f69998d7b3939cd5b93f54b177
James Farr <jfarr@zumac1.star.ucl.ac.uk>
2017-12-21 10:45:46 +0000

    Updates to repixelisation script and functions.

--- example_scripts/repixelise_class.py
+++ example_scripts/repixelise_class.py
@@ -2,10 +2,12 @@ import numpy as np
 from astropy.io import fits
 from astropy.table import Table
 import healpy as hp
-import split_pixels as split
-import WIP_split_pixels as WIP
 import WIP_split_pixels_class as WIP_class
 import time
+
+"""
+split_pixels(N_side,original_location,file_numbers,save_location,output_format,z_min)
+"""
 
 #Define the files that are to be repixelised.
 #Option 1: name each file individually.
@@ -18,8 +20,8 @@ import time
 #Set up a list of the files to import.
 original_file_location = '/Users/jfarr/Projects/repixelise/test_input'
 original_filename_structure = 'out_srcs_s0_{}.fits'
+file_numbers = [16]
 input_format = 'colore'
-file_numbers = [0]
 
 #Define the desired power of 2 for Nside for the output. This should be larger than that of the input.
 N_side_pow2 = 2
@@ -31,16 +33,17 @@ N_pix = 12*N_side**2
 z_min = 1.85
 
 #Define the directory that the output files should be saved to.
-save_location = '/Users/jfarr/Projects/repixelise/test_output'
-new_filename_structure = 'zmin_{}_nside_{}_pix_{}.fits'
+save_location = '/Users/jfarr/Projects/repixelise/test_output/WIP_class_output'
+new_filename_structure = '{}_zmin_{}_nside_{}_pix_{}.fits'
 output_format = 'picca'
 
-start = time.time()
-WIP.split_pixels(original_file_location,original_filename_structure,file_numbers,input_format,N_side,z_min,save_location + '/WIP_output',new_filename_structure,output_format)
-end = time.time()
-print("WIP method: ",end-start)
+#Define what the program should do if files with the proposed filename already exist. Options are:
+#0 - save as a separate file with a suffix _2 (and then _3 if _2 already exists etc.).
+#1 - if there are duplicate quasars, follow #0. Otherwise merge the existing file with the new data.
+#2 - Merge the existing file with the new data, avoiding duplicate objects.
+existing_file_option = 2
 
-#start = time.time()
-#WIP_class.split_pixels(original_file_location,original_filename_structure,file_numbers,N_side,z_min,save_location+'/WIP_class_output',new_filename_structure,output_format)
-#end = time.time()
-#print("WIP class method: ",end-start)
+start = time.time()
+WIP_class.split_pixels(original_file_location,original_filename_structure,file_numbers,input_format,N_side,save_location,new_filename_structure,output_format,z_min=z_min,existing_file_option=existing_file_option)
+end = time.time()
+print('Class method takes: {}s'.format(end-start))
