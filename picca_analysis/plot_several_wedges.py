e2a6f0a8880a4eac3fee4bdc0e2cdaa5279b35d7
James Farr <jfarr@zumac1.star.ucl.ac.uk>
2018-03-27 16:09:02 +0100

    Error correction

--- picca_analysis/plot_several_wedges.py
+++ picca_analysis/plot_several_wedges.py
@@ -52,9 +52,9 @@ for i in range(len(mubin_boundaries)-1):
 N_bins = len(mubins)
 
 if mode == 'plot_per_bin':
-    plot_functions.plot_per_bin(mubins,filenames,CAMB_sr)
+    plot_functions.plot_per_bin(mubins,filenames,CAMB_sr,plot_label_parameters)
 elif mode == 'plot_per_file':
-    plot_functions.plot_per_file(mubins,filenames,CAMB_sr)
+    plot_functions.plot_per_file(mubins,filenames,CAMB_sr,plot_label_parameters)
 else:
     error('Mode not recognised.')
 
