b318c3e1bb41138dd0a32469c8c7687da5cb52c1
James Farr <jfarr@cori10.nersc.gov>
2018-04-09 08:07:52 -0700

    Renamed the camb .txt files and tweaked plot functions

--- picca_analysis/plot_several_wedges.py
+++ picca_analysis/plot_several_wedges.py
@@ -20,7 +20,7 @@ for filename in filenames:
     print(filename)
 
 #Set the parameters to label plots by
-plot_label_parameters = ['correlation','quantity','sr']
+plot_label_parameters = ['correlation','quantities','sr']
 
 #Either plot_per_file (i.e. each file gets its own plot, showing all mu bins)
 #Or plot_per_bin (i.e. each mu bin has its own plot, but all files are grouped)
@@ -30,9 +30,9 @@ mode = 'plot_per_bin'
 quantity = 'gaussian'
 add_CAMB = True
 #CAMB_sr = ['10']
-CAMB_sr = ['10','20','40']
+CAMB_sr = ['1.0','2.0','4.0']
 scale_CAMB = [1.0,1.0,1.0]
-scale_CAMB = get_scales(filepaths)
+scale_CAMB = plot_functions.get_scales(filenames)
 
 #scale_CAMB = [2.9488*0.4084/0.9998, 3.4069*0.3534/0.9998 , 3.8682*0.3110/0.9998]
 #CAMB_location = '/Users/James/Projects/LyaCoLoRe/camb_scripts/'
@@ -45,9 +45,9 @@ mubin_boundaries = [-1.0,1.0]
 mubins = plot_functions.bins_from_boundaries(mubin_boundaries)
 
 if mode == 'plot_per_bin':
-    plot_functions.plot_per_bin(mubins,filenames,add_CAMB,plot_label_parameters,CAMB_sr=CAMB_sr,scale_CAMB=scale_CAMB)
+    plot_functions.plot_per_bin(mubins,filenames,add_CAMB,plot_label_parameters,CAMB_sr=CAMB_sr,scale_CAMB=scale_CAMB,CAMB_location=CAMB_location,CAMB_filename_format=CAMB_filename)
 elif mode == 'plot_per_file':
-    plot_functions.plot_per_file(mubins,filenames,add_CAMB,plot_label_parameters,CAMB_sr=CAMB_sr,scale_CAMB=scale_CAMB)
+    plot_functions.plot_per_file(mubins,filenames,add_CAMB,plot_label_parameters,CAMB_sr=CAMB_sr,scale_CAMB=scale_CAMB,CAMB_location=CAMB_location,CAMB_filename_format=CAMB_filename)
 else:
     error('Mode not recognised.')
 
