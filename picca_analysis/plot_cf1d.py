83f0e5aa97cccc13bd923ad4a10a9a806e311fc1
James Farr <jfarr@cori05.nersc.gov>
2018-04-13 02:23:02 -0700

    Added the ability to make a data/model plot to plot_cf1d

--- picca_analysis/plot_cf1d.py
+++ picca_analysis/plot_cf1d.py
@@ -38,14 +38,14 @@ separations = abs(R_cf1d - R_cf1d[:,None])
 binned_separations = (separations/bin_size).astype(int)
 #binned_separations = binned_separations - (binned_separations+1)*(binned_separations>=N_bins)
 
-R_binned = np.linspace(r_min+bin_size/2,r_max-bin_size/2,N_bins)
+r_binned = np.linspace(r_min+bin_size/2,r_max-bin_size/2,N_bins)
 xi_binned = np.zeros(N_bins)
 for n in range(N_bins):
     bin_entries = binned_separations==n
     N_contributions = np.sum(N_cf1d*bin_entries)
     xi_binned[n] = np.sum(bin_entries*DA*N_cf1d)/N_contributions
 
-plt.plot(R_binned,(R_binned**2)*xi_binned)
+plt.plot(r_binned,(r_binned**2)*xi_binned)
 plt.axhline(y=0,color='gray',ls=':')
 plt.xlabel('r [Mpc/h]')
 plt.ylabel('r^2 xi(r)')
@@ -54,7 +54,22 @@ plt.grid(True, which='both')
 CAMB_filepath = '/global/homes/j/jfarr/Projects/LyaCoLoRe/camb_scripts/camb_xi_4.0.txt'
 scale_CAMB = 1.0
 CAMB_sr = '4.0'
-plot_functions.add_CAMB_xi(CAMB_filepath,scale_CAMB,CAMB_sr)
+r_CAMB,xi_CAMB,plot_label_CAMB = plot_functions.get_CAMB_xi(CAMB_filepath,scale_CAMB,CAMB_sr)
+plt.plot(r_CAMB,xi_CAMB*(r_CAMB**2),label=plot_label_CAMB)
 
 plt.savefig('cf1d_xir2.pdf')
 plt.show()
+
+"""
+#Make a plot of data over model
+xi_CAMB_interp = np.interp(r_binned,r_CAMB,xi_CAMB)
+plt.plot(r_binned,xi_binned/xi_CAMB_interp,label='data/model')
+plt.axhline(y=0,color='gray',ls=':')
+plt.ylabel('r^2 xi(r)')
+plt.grid(True, which='both')
+plt.xlim(0,100)
+plt.ylim(-2,3)
+plt.legend()
+plt.savefig('data_over_model.pdf')
+plt.show()
+"""
