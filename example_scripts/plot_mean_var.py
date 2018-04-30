Corrected an error in calculation of prediction of sigma_F

example_scripts/plot_mean_var.py
@@ -4,22 +4,42 @@ import matplotlib.pyplot as plt
 import sys
 import process_functions as pf
 
-def read_file(basedir,nside,pix):
+lya = 1215.67
+
+def read_file(basedir,quantity,nside,pix):
     pix_100 = int(pix/100)
     dirname = basedir+'/'+str(pix_100)+'/'+str(pix)+'/'
     suffix = str(nside)+'-'+str(pix)+'.fits'
-    # file with delta flux for picca
-    filename = dirname+'/picca-gaussian-'+suffix
-    #print('picca flux file',filename)
-    h = fits.open(filename)
-    delta_rows = h[0].data.T
-    ivar_rows = h[1].data.T
-    loglam = h[2].data
-    h.close()
-    lya=1215.67
-    zs = (10**loglam)/lya - 1.0
-    flux_rows = (delta_rows+1)*(pf.get_mean_F_model(zs))
-    return zs,delta_rows,ivar_rows
+
+    if quantity == 'flux':
+        filename = dirname+'/transmission-'+suffix
+        h = fits.open(filename)
+        flux_rows = h[3].data.T
+        data_rows = flux_rows
+        lambdas = h[2].data
+        zs = lambdas/lya - 1.0
+        h.close()
+
+        filename = dirname+'/picca-{}-'.format(quantity)+suffix
+        #print('picca flux file',filename)
+        h = fits.open(filename)
+        ivar_rows = h[1].data.T
+        h.close()
+
+    else:
+        # file with delta flux for picca
+        filename = dirname+'/picca-{}-'.format(quantity)+suffix
+        #print('picca flux file',filename)
+        h = fits.open(filename)
+        delta_rows = h[0].data.T
+        ivar_rows = h[1].data.T
+        loglam = h[2].data
+        h.close()
+        zs = (10**loglam)/lya - 1.0
+
+        data_rows = delta_rows
+
+    return zs,data_rows,ivar_rows
 
 def get_means(data_rows,ivar_rows):
     N_skewers=data_rows.shape[0]
@@ -33,9 +53,15 @@ def get_means(data_rows,ivar_rows):
 
 # main folder where the processed files are
 basedir = '/global/cscratch1/sd/jfarr/LyaSkewers/CoLoRe_GAUSS/process_output_G_hZ_4096_32_sr2.0_bm1_biasG18_picos_nside16/'
+basedir = '/Users/jfarr/Projects/test_data/process_output_G_hZ_4096_32_sr2.0_bm1_nside16/'
+if len(sys.argv)>1:
+    quantity = sys.argv[1]
+else:
+    quantity = 'flux'
 nside = 16
 N_pixels = 100
 pixels = np.sort(np.random.choice(list(range(12*nside**2)),size=N_pixels))
+pixels = [1064,1096,1127,1128,1159,1160,1191,1192,1193,1223,1224,1225,1254,1255,1256,1257,1286,1287,1288,1289]
 
 # will combine pixel statistics
 sum_mean=None
@@ -44,7 +70,7 @@ sum_var=None
 
 for pix in pixels:
     # read file for particular pixel
-    zs,data_rows,ivar_rows=read_file(basedir,nside,pix)
+    zs,data_rows,ivar_rows=read_file(basedir,quantity,nside,pix)
 
     # compute statistics for pixel
     weights,mean,mean2 = get_means(data_rows,ivar_rows)
@@ -71,17 +97,31 @@ plt.plot(zs,overall_mean,label='mean')
 #plt.plot(zs,overall_var,label='variance')
 plt.plot(zs,overall_sigma,label='sigma')
 
-plt.title('Gaussian stats: mean and variance')
+plt.title('{} stats: mean and variance'.format(quantity))
 plt.xlabel('z')
 
 predicted_data = fits.open('input_files/tune_small_scale_fluctuations.fits')
 z_predicted = predicted_data[1].data['z']
-#mean_G_predicted = predicted_data[1].data['mean_G']
-sigma_G_predicted = predicted_data[1].data['sigma_G']
-#plt.plot(z_predicted,mean_G_predicted,label='predicted mean')
-plt.plot(z_predicted,sigma_G_predicted,label='predicted sigma')
+if quantity == 'gaussian':
+    sigma_G_predicted = predicted_data[1].data['sigma_G']
+    plt.plot(z_predicted,np.zeros(z_predicted.shape),label='predicted mean')
+    plt.plot(z_predicted,sigma_G_predicted,label='predicted sigma')
+elif quantity == 'flux':
+    mean_F_predicted = predicted_data[1].data['mean_F']
+    sigma_F_predicted = predicted_data[1].data['sigma_F']
+    plt.plot(z_predicted,mean_F_predicted,label='predicted mean')
+    plt.plot(z_predicted,sigma_F_predicted,label='predicted sigma')
+
+    mean_F_predicted_new = np.interp(zs,z_predicted,mean_F_predicted)
+    plt.plot(zs,overall_sigma/mean_F_predicted_new,label='integral / mean')
+    """
+    mean_F_model = predicted_data[1].data['mean_F_needed']
+    sigma_F_model = predicted_data[1].data['sigma_F_needed']
+    plt.plot(z_predicted,mean_F_model,label='model mean')
+    plt.plot(z_predicted,sigma_F_model,label='model sigma')
+    """
 
 plt.legend()
 plt.grid()
-plt.savefig('mean_var_gauss.pdf')
+plt.savefig('mean_var_{}.pdf'.format(quantity))
 plt.show()

example_scripts/process_colore_multi.py
@@ -310,6 +310,7 @@ This is done by
 
 #Work out sigma_G desired to achive the P1D sigma_F
 tuning_z_values = np.linspace(0,4.0,64)
+beta = 1.65
 
 if retune_small_scale_fluctuations == True:
 
@@ -317,7 +318,6 @@ if retune_small_scale_fluctuations == True:
 
     k = np.logspace(-5,10,10**5)
     D_values = np.interp(tuning_z_values,cosmology_data['Z'],cosmology_data['D'])
-    beta = 1.65
     sigma_G_tolerance = 0.0001
 
     def find_sigma_G(z,D,l,k,beta):
@@ -366,14 +366,13 @@ if retune_small_scale_fluctuations == True:
     plt.savefig('tune_flux_values_tol{}_n{}_Ferrors.pdf'.format(sigma_G_tolerance,tuning_z_values.shape[0]))
     plt.show()
     """
-    # TODO: add some kind of unique identifier?
-    #np.savetxt('input_files/tune_small_scale_fluctuations.txt',tune_small_scale_fluctuations)
+    # TODO: Add in beta in a header!
 
     prihdr = fits.Header()
     prihdu = fits.PrimaryHDU(header=prihdr)
     cols_DATA = fits.ColDefs(tune_small_scale_fluctuations)
     hdu_DATA = fits.BinTableHDU.from_columns(cols_DATA,name='DATA')
-    
+
     hdulist = fits.HDUList([prihdu, hdu_DATA])
     hdulist.writeto('input_files/tune_small_scale_fluctuations.fits')
     hdulist.close()
@@ -389,6 +388,7 @@ else:
 tuning_z_values = tune_small_scale_fluctuations['z']
 desired_sigma_G_values = tune_small_scale_fluctuations['sigma_G']
 desired_mean_F = tune_small_scale_fluctuations['mean_F']
+alphas = tune_small_scale_fluctuations['alpha']
 
 #desired_sigma_G_values = np.concatenate((2.0*np.ones(10),np.linspace(2.0,25.0,10)))
 #desired_sigma_G_values = 4.0*np.ones(20)
@@ -427,7 +427,7 @@ def produce_final_skewers(new_base_file_location,new_file_structure,new_filename
 
     #Add the physical density and flux skewers to the object.
     pixel_object.compute_physical_skewers() #Opportunity to change density type here
-    pixel_object.compute_flux_skewers() #Opportunity to vary A and alpha here
+    pixel_object.compute_flux_skewers(np.interp(pixel_object.Z,tuning_z_values,alphas),beta) #Opportunity to vary alpha and beta here
     times += [time.time()-np.sum(times)-start_time]
 
     if transmission_only == False:
@@ -450,7 +450,13 @@ def produce_final_skewers(new_base_file_location,new_file_structure,new_filename
     pixel_object.add_small_scale_gaussian_fluctuations(final_cell_size,tuning_z_values,extra_sigma_G_values,white_noise=True,lambda_min=0)
     times += [time.time()-np.sum(times)-start_time]
 
-    #create a table with DLAs
+    #If there are already physical/flux skewers, recalculate them.
+    if pixel_object.DENSITY_DELTA_rows is not None:
+        pixel_object.compute_physical_skewers()
+    if pixel_object.F_rows is not None:
+        pixel_object.compute_flux_skewers(np.interp(pixel_object.Z,tuning_z_values,alphas),beta)
+
+    #Add a table with DLAs in to the pixel object.
     if add_DLAs:
         pixel_object.add_DLA_table()
         times += [time.time()-np.sum(times)-start_time]