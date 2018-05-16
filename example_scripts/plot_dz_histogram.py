18e872362d61c2099a599e73d56fc28732159fa8
James Farr <jfarr@cori04.nersc.gov>
2018-05-16 09:00:32 -0700

    Modifications to plot_dz

--- example_scripts/plot_dz_histogram.py
+++ example_scripts/plot_dz_histogram.py
@@ -5,10 +5,14 @@ import glob
 
 basedir = 'example_data/update_160518/'
 basedir = '/Users/jfarr/Projects/test_data/process_output_G_hZsmooth_4096_32_sr2.0_bm1_biasG18_picos_nside16/'
+basedir = '/global/cscratch1/sd/jfarr/LyaSkewers/CoLoRe_GAUSS/test/'
 
 files = glob.glob(basedir + '/*/*/gaussian-colore-16*.fits')
 
 N_files = 100
+N_QSO = 10000
+N_cells = 10000
+
 if N_files < len(files):
     files = files[:N_files]
 
@@ -20,14 +24,20 @@ QSO_DZs = []
 vel_skw_values = []
 
 for file in files:
+    print(file)
     h = fits.open(file)
 
-    relevant_QSOs = [i for i in range(h[1].data.shape[0]) if h[1].data['Z_COSMO'][i] > z_min and h[1].data['Z_COSMO'][i] < z_max]
-    QSO_DZs += list(h[1].data['DZ_RSD'][relevant_QSOs])
+    if len(QSO_DZs)<N_QSO:
+        relevant_QSOs = [i for i in range(h[1].data.shape[0]) if h[1].data['Z_COSMO'][i] > z_min and h[1].data['Z_COSMO'][i] < z_max]
+        QSO_DZs += list(h[1].data['DZ_RSD'][relevant_QSOs])
+
 
     for relevant_QSO in relevant_QSOs:
-        relevant_cells = [i for i in range(h[4].data.shape[0]) if h[4].data['Z'][i] > z_min and h[4].data['Z'][i] < min(h[1].data['Z_COSMO'][relevant_QSO],z_max)]
-        vel_skw_values += list(h[3].data[relevant_QSO,relevant_cells])
+        if len(vel_skw_values)<N_cells:
+            relevant_cells = [i for i in range(h[4].data.shape[0]) if h[4].data['Z'][i] > z_min and h[4].data['Z'][i] < min(h[1].data['Z_COSMO'][relevant_QSO],z_max)]
+            vel_skw_values += list(h[3].data[relevant_QSO,relevant_cells])
+
+    h.close()
 
 print(len(QSO_DZs),len(vel_skw_values))
 
