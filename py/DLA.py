e525dc1f2d73dbc0ce703bc9657cf3d55530df11
James Farr <jfarr@zumac1.star.ucl.ac.uk>
2018-05-29 17:49:55 +0100

    Modifications to DLA and statistics file

--- py/DLA.py
+++ py/DLA.py
@@ -62,3 +62,65 @@ def get_N(z, Nmin=19.5, Nmax=22.0, nsamp=100):
     for i in range(0,len(probs)):
         N[i] = np.random.choice(nn,size=1,p=probs[i]/np.sum(probs[i]))
     return N
+
+def add_DLA_table_to_object(object,dla_bias=2.0):
+
+    y = interp1d(object.Z,object.D)
+    bias = dla_bias/(object.D)*y(2.25)
+
+    #We measure sigma_G already, but it is not fed back into the files at all. This should change.
+    sigma_g = object.SIGMA_G
+    #sigma_g = DLA.get_sigma_g(o.input_file)
+
+    nu_arr = nu_of_bD(bias*object.D)
+    flagged_pixels = flag_DLA(object.GAUSSIAN_DELTA_rows,nu_arr,sigma_g)
+
+    #Edges of the z bins
+    zedges = np.concatenate([[object.Z[0]],(object.Z[1:]+object.Z[:-1])*0.5,[object.Z[-1]+(-object.Z[-2]+object.Z[-1])*0.5]]).ravel()
+    z_width = zedges[1:]-zedges[:-1]
+
+    #Average number of DLAs per pixel
+    N = z_width*dNdz(object.Z)
+
+    #For a given z, probability of having the density higher than the threshold
+    p_nu_z = 1.0-norm.cdf(nu_arr)
+    mu = N/p_nu_z
+
+    #Should the "len(skewers)" be the number of skewers or the number of cells in each skewer here?
+    #Think it's the number of skewers but will check
+    pois = np.random.poisson(mu,size=(object.N_qso,len(mu)))
+    dlas = pois*flagged_pixels
+
+    ndlas = np.sum(dlas)
+    zdla = np.zeros(ndlas)
+    kskw = np.zeros(ndlas)
+    dz_dla = np.zeros(ndlas)
+    idx = 0
+
+    #In each skewer, look at each potential DLA.
+    for nskw,dla in enumerate(dlas):
+
+        #Asess which potential DLA position will be allocated a DLA.
+        ind = np.where(dla>0)[0]
+
+        #For each dla, assign it a redshift, a velocity and a column density.
+        for ii in ind:
+            zdla[idx:idx+dla[ii]] = np.random.uniform(low=(zedges[ii]),high=(zedges[ii+1]),size=dla[ii])
+            kskw[idx:idx+dla[ii]] = nskw
+            dz_dla[idx:idx+dla[ii]] = object.VEL_rows[nskw,ii]
+            idx = idx+dla[ii]
+
+    Ndla = get_N(zdla)
+    kskw = kskw.astype('int32')
+    MOCKIDs = object.MOCKID[kskw]
+
+    #Make the data into a table HDU
+    taux = astropy.table.Table([MOCKIDs,zdla,dz_dla,Ndla],names=('MOCKID','Z_DLA','DZ_DLA','N_HI_DLA'))
+
+    #Only include DLAs where the DLA is at lower z than the QSO
+    DLA_Z_QSOs = object.Z_QSO[kskw]
+    taux = taux[taux['Z_DLA']<DLA_Z_QSOs]
+
+    object.DLA_table = taux
+
+    return
