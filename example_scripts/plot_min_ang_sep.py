bcca6109df9811f82777d14d345adc4c6370c28d
James Farr <jfarr@zumac1.star.ucl.ac.uk>
2018-06-08 11:26:08 +0100

    Made the plotting process more efficient

--- example_scripts/plot_min_ang_sep.py
+++ example_scripts/plot_min_ang_sep.py
@@ -48,6 +48,8 @@ for i in range(1,N_qso_output-2):
         print('output file {} of {}'.format(i+1,N_qso_output-1),end='\r')
     RA_output_diff[i] = min(abs(RA_output_sorted[i]-RA_output_sorted[i+1]),abs(RA_output_sorted[i]-RA_output_sorted[i-1]))
     DEC_output_diff[i] = min(abs(DEC_output_sorted[i]-DEC_output_sorted[i+1]),abs(DEC_output_sorted[i]-DEC_output_sorted[i-1]))
+print('\n')
+
 
 #Get the RA and DEC diffs from the processed files
 master = fits.open(process_basedir + '/master.fits')
@@ -62,7 +64,7 @@ if N_qso:
     DEC_process = DEC_process[:N_qso]
 
 N_qso_process = RA_process.shape[0]
-print('\n{} QSOs in total process'.format(N_qso_process))
+print('{} QSOs in total process'.format(N_qso_process))
 
 RA_process_sorted = np.sort(RA_process)
 DEC_process_sorted = np.sort(DEC_process)
@@ -81,17 +83,27 @@ for i in range(1,N_qso_process-2):
         print('process file {} of {}'.format(i+1,N_qso_process-1),end='\r')
     RA_process_diff[i] = min(abs(RA_process_sorted[i]-RA_process_sorted[i+1]),abs(RA_process_sorted[i]-RA_process_sorted[i-1]))
     DEC_process_diff[i] = min(abs(DEC_process_sorted[i]-DEC_process_sorted[i+1]),abs(DEC_process_sorted[i]-DEC_process_sorted[i-1]))
-
+print('\n')
 
 #Make some plots
+print('calculating histograms')
 hist_bins=np.linspace(0.,0.01,10001)
+print('working on output RA..')
+Rod_hist=np.histogram(RA_output_diff,bins=hist_bins)
+print('working on output DEC..')
+Dod_hist=np.histogram(DEC_output_diff,bins=hist_bins)
+print('working on process RA..')
+Rpd_hist=np.histogram(RA_process_diff,bins=hist_bins)
+print('working on output DEC..')
+Dpd_hist=np.histogram(DEC_process_diff,bins=hist_bins)
+print('making plots...')
 
 #1. Plot with everything
 plt.figure(figsize=(12, 8), dpi= 80, facecolor='w', edgecolor='k')
-Rod_hist=plt.hist(RA_output_diff,bins=hist_bins,label='RA output',alpha=0.5)
-Dod_hist=plt.hist(DEC_output_diff,bins=hist_bins,label='DEC output',alpha=0.5)
-Rpd_hist=plt.hist(RA_process_diff,bins=hist_bins,label='RA process',alpha=0.5)
-Dpd_hist=plt.hist(DEC_process_diff,bins=hist_bins,label='DEC process',alpha=0.5)
+plt.bar(Rod_hist[1][:-1], Rod_hist[0], width=np.diff(Rod_hist[1]), align="edge")
+plt.bar(Dod_hist[1][:-1], Dod_hist[0], width=np.diff(Dod_hist[1]), align="edge")
+plt.bar(Rpd_hist[1][:-1], Rpd_hist[0], width=np.diff(Rpd_hist[1]), align="edge")
+plt.bar(Dpd_hist[1][:-1], Dpd_hist[0], width=np.diff(Dpd_hist[1]), align="edge")
 plt.xlim(-0.000005,0.0001)
 plt.grid()
 plt.legend()
@@ -100,8 +112,8 @@ plt.savefig('min_ang_sep_RD_op.pdf')
 
 #2. Plot the two RAs
 plt.figure(figsize=(12, 8), dpi= 80, facecolor='w', edgecolor='k')
-Rod_hist=plt.hist(RA_output_diff,bins=hist_bins,label='RA output',alpha=0.5)
-Rpd_hist=plt.hist(RA_process_diff,bins=hist_bins,label='RA process',alpha=0.5)
+plt.bar(Rod_hist[1][:-1], Rod_hist[0], width=np.diff(Rod_hist[1]), align="edge")
+plt.bar(Rpd_hist[1][:-1], Rpd_hist[0], width=np.diff(Rpd_hist[1]), align="edge")
 plt.xlim(-0.000005,0.0001)
 plt.grid()
 plt.legend()
@@ -110,8 +122,8 @@ plt.savefig('min_ang_sep_R_op.pdf')
 
 #3. Plot the two DECs
 plt.figure(figsize=(12, 8), dpi= 80, facecolor='w', edgecolor='k')
-Dod_hist=plt.hist(DEC_output_diff,bins=hist_bins,label='DEC output',alpha=0.5)
-Dpd_hist=plt.hist(DEC_process_diff,bins=hist_bins,label='DEC process',alpha=0.5)
+plt.bar(Dod_hist[1][:-1], Dod_hist[0], width=np.diff(Dod_hist[1]), align="edge")
+plt.bar(Dpd_hist[1][:-1], Dpd_hist[0], width=np.diff(Dpd_hist[1]), align="edge")
 plt.xlim(-0.000005,0.0001)
 plt.grid()
 plt.legend()
@@ -120,8 +132,8 @@ plt.savefig('min_ang_sep_D_op.pdf')
 
 #4. Plot the output data
 plt.figure(figsize=(12, 8), dpi= 80, facecolor='w', edgecolor='k')
-Rod_hist=plt.hist(RA_output_diff,bins=hist_bins,label='RA output',alpha=0.5)
-Dod_hist=plt.hist(DEC_output_diff,bins=hist_bins,label='DEC output',alpha=0.5)
+plt.bar(Rod_hist[1][:-1], Rod_hist[0], width=np.diff(Rod_hist[1]), align="edge")
+plt.bar(Dod_hist[1][:-1], Dod_hist[0], width=np.diff(Dod_hist[1]), align="edge")
 plt.xlim(-0.000005,0.0001)
 plt.grid()
 plt.legend()
@@ -130,8 +142,8 @@ plt.savefig('min_ang_sep_RD_o.pdf')
 
 #5. Plot the process data
 plt.figure(figsize=(12, 8), dpi= 80, facecolor='w', edgecolor='k')
-Rpd_hist=plt.hist(RA_process_diff,bins=hist_bins,label='RA process',alpha=0.5)
-Dpd_hist=plt.hist(DEC_process_diff,bins=hist_bins,label='DEC process',alpha=0.5)
+plt.bar(Rpd_hist[1][:-1], Rpd_hist[0], width=np.diff(Rpd_hist[1]), align="edge")
+plt.bar(Dpd_hist[1][:-1], Dpd_hist[0], width=np.diff(Dpd_hist[1]), align="edge")
 plt.xlim(-0.000005,0.0001)
 plt.grid()
 plt.legend()
