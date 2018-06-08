8d48ae22a91c32ebf3ce79ab30609d4a261d9891
James Farr <james.a.farr@btopenworld.com>
2018-06-08 09:29:07 +0100

    Corrected errors in code

--- example_scripts/plot_min_ang_sep.py
+++ example_scripts/plot_min_ang_sep.py
@@ -2,8 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from astropy.io import fits
 
+# TODO: Account for step from 0 to 2pi
+
 output_basedir = '/global/cscratch1/sd/jfarr/LyaSkewers/CoLoRe_GAUSS/output_G_hZsmooth_4096_32_sr2.0_bm1_biasG18_picos/'
 process_basedir = '/global/cscratch1/sd/jfarr/LyaSkewers/CoLoRe_GAUSS/process_output_G_hZsmooth_4096_32_sr2.0_bm1_biasG18_picos_nside16/'
+
+N_qso = 10**5
 
 #Get the RA and DEC diffs from the output files
 N_output_files = 32
@@ -11,10 +15,14 @@ RA_output = []
 DEC_output = []
 for i in range(N_output_files):
     print('output file {} of {}'.format(i+1,N_output_files),end='\r')
-    h = fits.open(output_basedir + 'out_srcs_s1_{}.fits'.format(i))
-    RA_output = np.concatenate((RA_output,h[1].data['RA']))
-    DEC_output = np.concatenate((DEC_output,h[1].data['DEC']))
-    h.close()
+    if RA_output.shape[0]<N_qso:
+        h = fits.open(output_basedir + 'out_srcs_s1_{}.fits'.format(i))
+        RA_output = np.concatenate((RA_output,h[1].data['RA']))
+        DEC_output = np.concatenate((DEC_output,h[1].data['DEC']))
+        h.close()
+
+RA_output = RA_output[:N_qso]
+DEC_output = DEC_output[:N_qso]
 
 N_qso_output = RA_output.shape[0]
 print('\n{} QSOs in total output'.format(N_qso_output))
@@ -26,12 +34,17 @@ RA_output_diff = np.zeros(N_qso_output-1)
 DEC_output_diff = np.zeros(N_qso_output-1)
 
 print('calculating separations')
-for i in range(N_qso_output-1):
-    if i//1000 ==i/1000:
-        print('output file {} of {}'.format(i+1,N_qso_output-1),end='\r')
-    RA_output_diff[i] = abs(RA_output_sorted[i]-RA_output_sorted[i+1])
-    DEC_output_diff[i] = abs(DEC_output_sorted[i]-DEC_output_sorted[i+1])
 
+RA_output_diff[0] = min(abs(RA_output_sorted[0]-RA_output_sorted[1],abs(RA_output_sorted[0]-RA_output_sorted[-1]+360))
+RA_output_diff[-1] = min(abs(RA_output_sorted[-1]-RA_output_sorted[0]-360.,abs(RA_output_sorted[-1]-RA_output_sorted[-2]))
+DEC_output_diff[0] = min(abs(DEC_output_sorted[0]-DEC_output_sorted[1],abs(DEC_output_sorted[0]-DEC_output_sorted[-1]+360))
+DEC_output_diff[-1] = min(abs(DEC_output_sorted[-1]-DEC_output_sorted[0]-360.,abs(DEC_output_sorted[-1]-DEC_output_sorted[-2]))
+
+for i in range(1,N_qso_output-2):
+    if i//1000 == i/1000:
+        print('output file {} of {}'.format(i+1,N_qso_output-1),end='\r')
+    RA_output_diff[i] = min(abs(RA_output_sorted[i]-RA_output_sorted[i+1]),abs(RA_output_sorted[i]-RA_output_sorted[i-1]))
+    DEC_output_diff[i] = min(abs(DEC_output_sorted[i]-DEC_output_sorted[i+1]),abs(DEC_output_sorted[i]-DEC_output_sorted[i-1]))
 
 #Get the RA and DEC diffs from the processed files
 master = fits.open(process_basedir + '/master.fits')
@@ -40,6 +53,9 @@ RA_process = master[1].data['RA']
 DEC_process = master[1].data['DEC']
 
 master.close()
+
+RA_process = RA_process[:N_qso]
+DEC_process = DEC_process[:N_qso]
 
 N_qso_process = RA_process.shape[0]
 print('\n{} QSOs in total process'.format(N_qso_process))
@@ -50,12 +66,17 @@ DEC_process_sorted = np.sort(DEC_process)
 RA_process_diff = np.zeros(N_qso_process-1)
 DEC_process_diff = np.zeros(N_qso_process-1)
 
+RA_process_diff[0] = min(abs(RA_process_sorted[0]-RA_process_sorted[1],abs(RA_process_sorted[0]-RA_process_sorted[-1]+360))
+RA_process_diff[-1] = min(abs(RA_process_sorted[-1]-RA_process_sorted[0]-360.,abs(RA_process_sorted[-1]-RA_process_sorted[-2]))
+DEC_process_diff[0] = min(abs(DEC_process_sorted[0]-DEC_process_sorted[1],abs(DEC_process_sorted[0]-DEC_process_sorted[-1]+360))
+DEC_process_diff[-1] = min(abs(DEC_process_sorted[-1]-DEC_process_sorted[0]-360.,abs(DEC_process_sorted[-1]-DEC_process_sorted[-2]))
+
 print('calculating separations')
-for i in range(N_qso_process-1):
+for i in range(1,N_qso_process-2):
     if i//1000 ==i/1000:
-        print('output file {} of {}'.format(i+1,N_qso_output-1),end='\r')
-    RA_process_diff[i] = abs(RA_process_sorted[i]-RA_process_sorted[i+1])
-    DEC_process_diff[i] = abs(DEC_process_sorted[i]-DEC_process_sorted[i+1])
+        print('process file {} of {}'.format(i+1,N_qso_process-1),end='\r')
+    RA_process_diff[i] = min(abs(RA_process_sorted[i]-RA_process_sorted[i+1]),abs(RA_process_sorted[i]-RA_process_sorted[i-1]))
+    DEC_process_diff[i] = min(abs(DEC_process_sorted[i]-DEC_process_sorted[i+1]),abs(DEC_process_sorted[i]-DEC_process_sorted[i-1]))
 
 
 #Make some plots
