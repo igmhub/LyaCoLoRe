e77f412709655574e94d9d18845c2ab29ca4072c
James Farr <james.a.farr@btopenworld.com>
2018-06-17 22:15:35 +0100

    Added script to put picca output into wedges

---
+++ example_scripts/convert_to_wedge.py
@@ -0,0 +1,48 @@
+import numpy as np
+from astropy.io import fits
+
+filename = ''
+N_bins = 40
+R_min = 0.
+R_max = 160.
+mu_bins = np,array([(0.0,0.1),(0.45,0.55),(0.9,1.0)])
+
+R_bin_size = (R_max - R_min)/N_bins
+R_edges = np.linspace(R_min,R_max,N_bins+1)
+
+RP_bins = h[1].data['RP']
+RT_bins = h[1].data['RT']
+DA = h[2].data['DA']
+WE = h[2].data['WE']
+
+DA_mean = np.average(DA,weights=WE,axis=0)
+DA_mean_squared = np.average(DA**2,weights=WE,axis=0)
+DA_var = DA_mean_squared - DA_mean**2
+WE_sum = np.sum(WE,axis=0)
+
+R_original = np.sqrt(RP_bins**2 + RT_bins**2)
+mu_original = R_original/RT_bins
+
+R_bin_numbers = np.array(list(range(N_bins)))
+binned_R_numbers = np.digitize(R_original,R_edges) - 1
+binned_R_values = (binned_R_numbers + 0.5) * R_bin_size
+
+binned_data = np.zeros((mu_bins.shape[0],R_bins.shape[0]))
+
+for i,mu_bin in enumerate(mu_bins):
+    mu_min = mu_bin[0]
+    mu_max = mu_bin[1]
+
+    mu_relevant = (mu_original > mu_min)*(mu_original < mu_max)
+
+    for j,R_bin in enumerate(R_bins):
+
+        relevant = mu_relevant * (binned_R == R_bin)
+
+        binned_data[i,j] = np.average(DA_mean[relevant],weights=WE_sum[relevant])
+
+    plt.plot(binned_R_values,binned_data[i,:],label=str(mu_bin))
+
+plt.legend()
+plt.grid()
+plt.show()
