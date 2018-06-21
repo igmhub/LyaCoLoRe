100369ad78652d34149d6c3ae1f0d9fad1cbc6e8
James Farr <jfarr@cori11.nersc.gov>
2018-06-21 10:00:04 -0700

    Updated fully parallel process

--- run_process_colore_multi_node.sh
+++ run_process_colore_multi_node.sh
@@ -1,6 +1,7 @@
 # specify number of nodes and cores to use
-NNODES=32
+NNODES=64
 NCORES=64
+TIME="00:20:00" #hh:mm:ss
 
 # specify process parameters
 NSIDE=16
@@ -29,13 +30,14 @@ echo "${NFILES} input files have been found"
 
 # full path to folder where output will be written
 OUTPUT_PATH="/global/cscratch1/sd/jfarr/LyaSkewers/CoLoRe_GAUSS/process_output_G_hZsmooth_${COLORE_NGRID}_${COLORE_NODES}_sr${R_SMOOTH}_bm1_biasG18_picos_nside${NSIDE}/"
+OUTPUT_PATH="/global/cscratch1/sd/jfarr/LyaSkewers/CoLoRe_GAUSS/test_4/"
 echo "output will written to "$OUTPUT_PATH
 if [ ! -d $OUTPUT_PATH ] ; then
     mkdir -p $OUTPUT_PATH
 fi
 echo "output logs will be saved to "$OUTPUT_PATH"/logs"
-if [ ! -d $outdir/logs ] ; then
-    mkdir -p $outdir/logs
+if [ ! -d $OUTPUT_PATH/logs ] ; then
+    mkdir -p $OUTPUT_PATH/logs
 fi
 
 # full path to file with tuning sigma_G data
@@ -45,12 +47,19 @@ TUNING_PATH="/global/homes/j/jfarr/Projects/LyaCoLoRe/input_files/tune_small_sca
 RUN_FILE="/global/homes/j/jfarr/Projects/LyaCoLoRe/run_files/process_colore_output_G_hZsmooth_${COLORE_NGRID}_${COLORE_NODES}_sr${R_SMOOTH}_bm1_biasG18_picos.sh"
 echo "run file "$RUN_FILE
 
+# make master file and new file structure
+date
+echo "making master file"
+${PROCESS_PATH}/make_master.py --in-dir ${INPUT_PATH} --out-dir ${OUTPUT_PATH} --nside ${NSIDE} --nproc ${NCORES} --min-cat-z ${MIN_CAT_Z}
+wait
+date
+
 cat > $RUN_FILE <<EOF
 #!/bin/bash -l
 
-#SBATCH --partition regular
-#SBATCH --nodes ${NFILES}
-#SBATCH --time 00:30:00
+#SBATCH --partition debug
+#SBATCH --nodes ${NNODES}
+#SBATCH --time ${TIME}
 #SBATCH --job-name process_colore
 #SBATCH --error "/global/homes/j/jfarr/Projects/LyaCoLoRe/run_files/process-colore-%j.err"
 #SBATCH --output "/global/homes/j/jfarr/Projects/LyaCoLoRe/run_files/process-colore-%j.out"
@@ -60,33 +69,49 @@ cat > $RUN_FILE <<EOF
 umask 0002
 export OMP_NUM_THREADS=64
 
-date
+PIXDIRS=\`\ls -tr1d ${OUTPUT_PATH}/[0-9]*/*\`
+NPIXELS=\`echo \$PIXDIRS | wc -w\`
+PIXDIRS_list=(\$PIXDIRS)
 
-echo "making master file"
-${PROCESS_PATH}/make_master.py --in-dir ${INPUT_PATH} --out-dir ${OUTPUT_PATH} --nside ${NSIDE} --nproc ${NCORES} --min-cat-z ${MIN_CAT_Z}
+PIXELS=()
+for PIXDIR in \$PIXDIRS ; do 
+    PIX=\${PIXDIR##*/}
+    PIXELS=("\${PIXELS[@]}" \$PIX)
+done
 
-date
+NPIXELS_PER_NODE=\$(( (\$NPIXELS + $NNODES - 1)/$NNODES ))
+START_INDEX=0
+STOP_INDEX=\$(( \$NPIXELS_PER_NODE - 1 ))
+FINAL_NODE=0
 
-NPIXELS=$(( 12*$NSIDE*$NSIDE ))
-NPIXELS_PER_NODE=$(( $NPIXELS/$NNODES + 1 ))
-PIXEL_START=1
-PIXEL_STOP=$NPIXELS_PER_NODE
+for NODE in \`seq $NNODES\` ; do
+    echo "starting node \$NODE"
 
-for NODE in `seq $NNODES` ; do
-    echo "starting node $NODE"
+    NODE_PIXELS=\${PIXELS[@]:\$START_INDEX:\$NPIXELS_PER_NODE}
 
-    # list of files to run
-    if (( $NODE == $NNODES )) ; then
-        last=""
+    echo "looking at pixels: \${NODE_PIXELS}"
+
+    command="srun -N 1 -n 1 -c ${NCORES} ${PROCESS_PATH}/make_transmission.py --in-dir ${INPUT_PATH} --out-dir ${OUTPUT_PATH} --pixels \${NODE_PIXELS} --tuning-file ${TUNING_PATH} --nside ${NSIDE} --nproc ${NCORES} --IVAR-cut ${IVAR_CUT} --cell-size ${CELL_SIZE} --lambda-min ${LAMBDA_MIN} ${FLAGS}"
+
+    echo \$command
+    \$command >& ${OUTPUT_PATH}/logs/node-\${NODE}.log &
+
+    if (( \$FINAL_NODE == 1)) ; then
+        echo "all pixels allocated, no more nodes needed"
+        break
     fi
-    echo ${PIXEL_START}-${PIXEL_STOP}
-    PIXEL_START=$(( PIXEL_START + NPIXELS_PER_NODE ))
-    PIXEL_STOP=$(( PIXEL_STOP + NPIXELS_PER_NODE ))
 
-    command="srun -N 1 -n 1 -c ${NCORES} ${PROCESS_PATH}/make_transmission.py --in-dir ${INPUT_PATH} --out-dir ${OUTPUT_PATH} --pix-start ${PIXEL_START} --pix-stop ${PIXEL_STOP} --tuning-file ${TUNING_PATH} --nside ${NSIDE} --nproc ${NCORES} --IVAR-cut ${IVAR_CUT} --cell-size ${CELL_SIZE} --lambda-min ${LAMBDA_MIN} ${FLAGS}"
+    START_INDEX=\$(( \$STOP_INDEX + 1 ))
+    STOP_INDEX=\$(( \$START_INDEX + \$NPIXELS_PER_NODE - 1))
 
-    echo $command
-    $command >& $outdir/logs/node-$node.log &
+    if (( \$STOP_INDEX >= (\$NPIXELS - 1) )) ; then
+        STOP_INDEX=\$NPIXELS-1
+        FINAL_NODE=1
+    fi
+
+done
+
+wait
 
 date
 
