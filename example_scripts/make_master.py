7f92e8f32ec9f948bc7ec29643310404993dd966
James Farr <jfarr@zumac1.star.ucl.ac.uk>
2018-06-29 09:35:30 +0100

    Implemented a first version of adding correlated small scale power

--- example_scripts/make_master.py
+++ example_scripts/make_master.py
@@ -83,8 +83,8 @@ else:
     N_pix = 12*N_side**2
 
 #Define the original file structure
-original_filename_structure = 'out_srcs_s1_{}.fits' #file_number
-file_numbers = list(range(0,32))
+original_filename_structure = 'N1000_out_srcs_s1_{}.fits' #file_number
+file_numbers = list(range(0,1))
 input_format = 'gaussian_colore'
 
 #Set file structure
