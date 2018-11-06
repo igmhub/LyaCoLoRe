9527ea666e454d55f043afad1f5c0dbcbaa3d6bc
James Farr <jfarr@zumac1.star.ucl.ac.uk>
2018-11-06 18:56:02 +0000

    New plotting script

---
+++ picca_analysis/plot_picca.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy.io import fits
+import h5py
+import glob
+
+default_location = ['/global/homes/j/jfarr/Programs/picca/picca_00000/']
+
+if len(sys.argv) > 0:
+    locations = sys.argv[1:]
+else:
+    locations = default_location
+
+#Plotting decisions
+plot_system = 'plot_per_file' #per bin or per file
+mu_boundaries = [0.0,0.5,0.8,0.95,1.0]
+model = 'Slosar11'
+include_fits = True
+r_power = 2
+
+#Get the correlation objects
+corr_objects = plot_functions.get_correlation_objects(locations)
+
+#Make plots of the objects
+plot_functions.make_plots(corr_objects,mu_boundaries,plot_system,r_power,include_fits)
