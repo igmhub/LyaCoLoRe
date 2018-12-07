b185208372a296ab2c69574724cdfb0f9bd0b39c
James Farr <jfarr@cori10.nersc.gov>
2018-12-07 08:19:19 -0800

    Minor changes

--- py/Pk1D.py
+++ py/Pk1D.py
@@ -48,8 +48,10 @@ def get_Pk1D(skewer_rows,IVAR_rows,R_hMpc,z,z_value=0.0,z_width=None,units='km/s
         pk_rows = np.abs(ft_rows)**2
 
         #compute Fourier transform of Top-Hat filter of size l_kms and apply it
-        W_kms = np.sinc((k_kms*dv_kms)/(2*np.pi))
-        pk_rows /= (W_kms**2)
+        R1 = 10.0 #kms
+        W_kms = (np.sinc((k_kms*dv_kms)/(2*np.pi)))**2 #* np.exp(-pow(k_kms*R1,2))
+        pk_rows /= (W_kms)
+        #print(W_kms[-5:]**2)
 
         #calculate mean and variance
         pk_kms = np.average(pk_rows,axis=0)
