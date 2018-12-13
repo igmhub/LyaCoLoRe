59da1c52b0f8a6c254997699316aeafe2705182d
James Farr <james.a.farr@btopenworld.com>
2018-12-13 10:37:37 +0100

    Updated save methods; added renormalise/rebinning functions; improved summaries script

--- py/stats.py
+++ py/stats.py
@@ -40,25 +40,29 @@ def combine_pixel_means(results):
     return N, mean_DELTA, var_DELTA
 
 #Function to take a list of sets of statistics (as produced by 'get_statistics'), and calculate means and variances.
-def combine_means(means_list):
+def combine_statistics(statistics_list):
 
-    means_shape = means_list[0].shape
-    means_data_type = means_list[0].dtype
-    N_cells = means_shape[0]
+    statistics_shape = statistics_list[0].shape
+    statistics_data_type = statistics_list[0].dtype
+    N_cells = statistics_shape[0]
 
-    quantities = ['GAUSSIAN_DELTA','GAUSSIAN_DELTA_SQUARED','DENSITY_DELTA','DENSITY_DELTA_SQUARED','F','F_SQUARED','F_DELTA','F_DELTA_SQUARED']
+    quantities = [('N', 'f4')
+        , ('GAUSSIAN_DELTA_MEAN', 'f4'), ('GAUSSIAN_DELTA_VAR', 'f4')
+        , ('DENSITY_DELTA_MEAN', 'f4'), ('DENSITY_DELTA_VAR', 'f4')
+        , ('TAU_MEAN', 'f4'), ('TAU_VAR', 'f4')
+        , ('F_MEAN', 'f4'), ('F_VAR', 'f4')]
 
-    combined_means = np.zeros(means_shape,dtype=means_data_type)
-    for means_array in means_list:
-        combined_means['N'] += means_array['N']
+    combined_statistics = np.zeros(statistics_shape,dtype=means_data_type)
+    for s_array in statistics_list:
+        combined_means['N'] += s_array['N']
 
-    for i in range(N_cells):
-        if combined_means['N'][i] > 0:
-            for quantity in quantities:
-                for means_array in means_list:
-                    combined_means[quantity][i] += (means_array['N'][i]*means_array[quantity][i])/combined_means['N'][i]
+    #Combine the means.
+    cells = combined_means['N']>0
+    for q in quantities:
+        for s_array in statistics_list:
+            combined_statistics[quantity][cells] += s_array['N'][cells]*s_array[q][cells]/combined_statistics['N'][cells]
 
-    return combined_means
+    return combined_statistics
 
 #Function to convert a set of means of quantities and quantities squared (as outputted by 'combine_means') to a set of means and variances.
 def means_to_statistics(means):
@@ -66,38 +70,40 @@ def means_to_statistics(means):
     statistics_dtype = [('N', 'f4')
         , ('GAUSSIAN_DELTA_MEAN', 'f4'), ('GAUSSIAN_DELTA_VAR', 'f4')
         , ('DENSITY_DELTA_MEAN', 'f4'), ('DENSITY_DELTA_VAR', 'f4')
-        , ('F_MEAN', 'f4'), ('F_VAR', 'f4')
-        , ('F_DELTA_MEAN', 'f4'), ('F_DELTA_VAR', 'f4')]
+        , ('TAU_MEAN', 'f4'), ('TAU_VAR', 'f4')
+        , ('F_MEAN', 'f4'), ('F_VAR', 'f4')]
+        #, ('F_DELTA_MEAN', 'f4'), ('F_DELTA_VAR', 'f4')]
 
     statistics = np.zeros(means.shape,dtype=statistics_dtype)
 
     statistics['N'] = means['N']
     statistics['GAUSSIAN_DELTA_MEAN'] = means['GAUSSIAN_DELTA']
     statistics['DENSITY_DELTA_MEAN'] = means['DENSITY_DELTA']
+    statistics['TAU_MEAN'] = means['TAU']
     statistics['F_MEAN'] = means['F']
-    statistics['F_DELTA_MEAN'] = means['F_DELTA']
+    #statistics['F_DELTA_MEAN'] = means['F_DELTA']
 
     statistics['GAUSSIAN_DELTA_VAR'] = means['GAUSSIAN_DELTA_SQUARED'] - means['GAUSSIAN_DELTA']**2
     statistics['DENSITY_DELTA_VAR'] = means['DENSITY_DELTA_SQUARED'] - means['DENSITY_DELTA']**2
+    statistics['TAU_VAR'] = means['TAU_SQUARED'] - means['TAU']**2
     statistics['F_VAR'] = means['F_SQUARED'] - means['F']**2
-    statistics['F_DELTA_VAR'] = means['F_DELTA_SQUARED'] - means['F_DELTA']**2
+    #statistics['F_DELTA_VAR'] = means['F_DELTA_SQUARED'] - means['F_DELTA']**2
 
     return statistics
 
 #Function to write the statistics data to file, along with an HDU extension contanint cosmology data.
-def write_statistics(location,N_side,statistics,cosmology_data):
+def write_statistics(location,filename,statistics):
 
     #Construct HDU from the statistics array.
     prihdr = fits.Header()
     prihdu = fits.PrimaryHDU(header=prihdr)
     cols_stats = fits.ColDefs(statistics)
     hdu_stats = fits.BinTableHDU.from_columns(cols_stats,name='STATISTICS')
-    cols_cosmology = fits.ColDefs(cosmology_data)
-    hdu_cosmology = fits.BinTableHDU.from_columns(cols_cosmology,name='COSMO')
+    #cols_cosmology = fits.ColDefs(cosmology_data)
+    #hdu_cosmology = fits.BinTableHDU.from_columns(cols_cosmology,name='COSMO')
 
     #Put the HDU into an HDUlist and save as a new file. Close the HDUlist.
-    filename = '/statistics.fits'
-    hdulist = fits.HDUList([prihdu,hdu_stats,hdu_cosmology])
+    hdulist = fits.HDUList([prihdu,hdu_stats])
     hdulist.writeto(location+filename)
     hdulist.close
 
--- scripts/make_summaries.py
+++ scripts/make_summaries.py
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import numpy as np
-from astropy.io import fits
-
-from pyacolore import DLA
-
-parser.add_argument('--base-dir', type = str, default = None, required=True,
-                    help = 'base data directory')
-=======
 #!/usr/bin/env python
 
 import numpy as np
@@ -28,7 +19,6 @@ parser.add_argument('--base-dir', type = str, default = None, required=True,
 
 parser.add_argument('--nproc', type = int, default = 1, required=False,
                     help = 'number of processes to use')
->>>>>>> 9a44f4319e733c0ca255cf9d37c08b596cbfca0b
 
 parser.add_argument('--nside', type = int, default = 16, required=False,
                     help = 'HEALPix nside for output files (must be 2^n)')
@@ -36,21 +26,72 @@ parser.add_argument('--nside', type = int, default = 16, required=False,
 parser.add_argument('--pixels', type = int, default = None, required=False,
                     help = 'which pixel numbers to work on', nargs='*')
 
-<<<<<<< HEAD
-base_dir = args.base_dir
-=======
+parser.add_argument('--picca_N_merge', type = int, default = None, required=False,
+                    help = 'number of cells to merge when rebinning for picca files')
+
 args = parser.parse_args()
 
 base_dir = args.base_dir
 N_processes = args.nproc
->>>>>>> 9a44f4319e733c0ca255cf9d37c08b596cbfca0b
 N_side = args.nside
 pixels = args.pixels
 if not pixels:
     pixels = list(range(12*N_side**2))
+N_merge = args.picca_N_merge
 
-<<<<<<< HEAD
-=======
+################################################################################
+"""
+Make the DLA master file.
+"""
+
 #Make the DLA master file
->>>>>>> 9a44f4319e733c0ca255cf9d37c08b596cbfca0b
 DLA.make_DLA_master(base_dir,N_side,pixels)
+
+################################################################################
+"""
+Make the global statistics file.
+"""
+
+# TODO: paralellise
+#Get the statistics from all pixels.
+statistics_list = []
+for pixel in pixels:
+    s_filename = utils.get_file_name(base_dir,'statistics',N_side,pixel)
+    s = fits.open(dirname+s_filename)
+    statistics_list += s[1].data
+    s.close()
+
+#Combine the statistics from all of the pixels.
+statistics = combine_statistics(statistics_list)
+
+#Save the final file.
+filename = 'statistics.fits'
+stats.write_statistics(base_dir,filename,statistics)
+
+################################################################################
+"""
+Renormalise the picca files using the global statistics data.
+Also rebin the files to get the desired cell_size.
+"""
+
+#Also need to add in the rebinned ones? Or should we rebin here?
+#Issue  with different names in the statistics files?
+quantities = ['tau','flux']
+stats_quantities = ['TAU','F']
+
+# TODO: paralellise
+#For each pixel, and each quantity, renormalise the picca file
+for pixel in pixels:
+
+    #Open up the per-pixel stats file
+    dirname = utils.get_dir_name(base_dir,pixel)
+    s_filename = utils.get_file_name(base_dir,'statistics',N_side,pixel)
+    s = fits.open(dirname+s_filename)
+
+    for i,q in enumerate(quantities):
+
+        #Get the old mean, and renormalise.
+        # TODO: this use of "stats_quantities" is v ugly
+        old_mean = s[1].data[stats_quantities[i]+'_MEAN']
+        filename = utils.get_file_name(base_dir,'picca-'+q,N_side,pixel)
+        utils.renormalise_picca_file(filepath,old_mean,new_mean,N_merge=N_merge)
