4568deb0d309910bab86f0147e6435c9d585b857
James Farr <james.a.farr@btopenworld.com>
2019-02-13 18:18:29 +0000

    First attempt at adding function to get b_nu

--- py/bias.py
+++ py/bias.py
@@ -42,7 +42,51 @@ def get_bias_delta(data,betas,z_values,d=0.001,z_width=0.2):
         mean_F_over = overdensity.get_mean_quantity('flux',z_value=z_value,z_width=z_width,single_value=False,power=1)
         mean_F_under = underdensity.get_mean_quantity('flux',z_value=z_value,z_width=z_width,single_value=False,power=1)
         mean_F = data.get_mean_quantity('flux',z_value=z_value,z_width=z_width,single_value=False,power=1)
-        
+
+        i_lower = np.searchsorted(data.Z,z_value - z_width/2.)
+        i_upper = np.searchsorted(data.Z,z_value + z_width/2.)
+        D_values = data.D[i_lower:i_upper]
+        #print(np.sum(data.IVAR_rows[:,i_lower:i_lower+5],axis=0),np.sum(data.IVAR_rows[:,i_upper-5:i_upper],axis=0))
+
+        #D_value = np.interp(z_value,data.Z,data.D)
+        bias = np.average((1/mean_F) * (1/(2.*d*D_values)) * (mean_F_over - mean_F_under))
+        biases += [bias]
+
+    biases = np.array(biases)
+
+    return biases
+
+#Function to get the different RSD weights for calculating bias_nu.
+
+
+#Function to get the bias of eta at various z values from a sim data object.
+#Assumes that the object already has tau calculated and RSDs applied.
+def get_bias_nu(data,betas,z_values,d=0.001,z_width=0.2,z_r0=2.5):
+
+    #Find the pivot distance, and calculate extra velocity.
+    r0 = np.interp(z_r0,data.Z,data.R)
+    dv = (data.R - r0) * d
+
+    #Add small extra grad to velocity skewers.
+    grad_increase = copy.deepcopy(data)
+    grad_increase.VEL_rows += dv
+    grad_increase.add_all_RSDs(thermal=include_thermal_effects)
+
+    #Subtract small extra grad to velocity skewers.
+    grad_decrease = copy.deepcopy(data)
+    grad_decrease.VEL_rows -= dv
+    grad_decrease.add_all_RSDs(thermal=include_thermal_effects)
+
+    #Calculate mean fluxes in under and overdensities, as well as normal
+    biases = []
+    for z_value in z_values:
+
+        #We get means across the z-chunk and combine once bias has been computed.
+        #This avoids overweighting the low end of the chunk.
+        mean_F_over = overdensity.get_mean_quantity('flux',z_value=z_value,z_width=z_width,single_value=False,power=1)
+        mean_F_under = underdensity.get_mean_quantity('flux',z_value=z_value,z_width=z_width,single_value=False,power=1)
+        mean_F = data.get_mean_quantity('flux',z_value=z_value,z_width=z_width,single_value=False,power=1)
+
         i_lower = np.searchsorted(data.Z,z_value - z_width/2.)
         i_upper = np.searchsorted(data.Z,z_value + z_width/2.)
         D_values = data.D[i_lower:i_upper]
