0eedf4d39e8f56fc778761809ec02087fba2bb57
James Farr <jfarr@cori04.nersc.gov>
2019-02-13 09:46:22 -0800

    Removed issue with overweighting bottom of chunk

--- py/bias.py
+++ py/bias.py
@@ -34,20 +34,24 @@ def get_bias_delta(data,betas,z_values,d=0.001,z_width=0.2):
     underdensity.lya_absorber.tau /= np.exp(betas*underdensity.D*d)
 
     #Calculate mean fluxes in under and overdensities, as well as normal
-    mean_F_over = []
-    mean_F_under = []
-    mean_F = []
+    biases = []
     for z_value in z_values:
-        mean_F_over += [overdensity.get_mean_quantity('flux',z_value=z_value,z_width=z_width,single_value=True,power=1)]
-        mean_F_under += [underdensity.get_mean_quantity('flux',z_value=z_value,z_width=z_width,single_value=True,power=1)]
-        mean_F += [data.get_mean_quantity('flux',z_value=z_value,z_width=z_width,single_value=True,power=1)]
 
-    mean_F_over = np.array(mean_F_over)
-    mean_F_under = np.array(mean_F_under)
-    mean_F = np.array(mean_F)
+        #We get means across the z-chunk and combine once bias has been computed.
+        #This avoids overweighting the low end of the chunk.
+        mean_F_over = overdensity.get_mean_quantity('flux',z_value=z_value,z_width=z_width,single_value=False,power=1)
+        mean_F_under = underdensity.get_mean_quantity('flux',z_value=z_value,z_width=z_width,single_value=False,power=1)
+        mean_F = data.get_mean_quantity('flux',z_value=z_value,z_width=z_width,single_value=False,power=1)
+        
+        i_lower = np.searchsorted(data.Z,z_value - z_width/2.)
+        i_upper = np.searchsorted(data.Z,z_value + z_width/2.)
+        D_values = data.D[i_lower:i_upper]
+        #print(np.sum(data.IVAR_rows[:,i_lower:i_lower+5],axis=0),np.sum(data.IVAR_rows[:,i_upper-5:i_upper],axis=0))
 
-    #Calculate bias using gradient
-    D_values = np.interp(z_values,data.Z,data.D)
-    b = (1/mean_F) * (1/(2.*d*D_values)) * (mean_F_over - mean_F_under)
+        #D_value = np.interp(z_value,data.Z,data.D)
+        bias = np.average((1/mean_F) * (1/(2.*d*D_values)) * (mean_F_over - mean_F_under))
+        biases += [bias]
 
-    return b
+    biases = np.array(biases)
+
+    return biases
