7aba038010c38e4353075714e793792f4d73065b
James Farr <jfarr@cori05.nersc.gov>
2019-03-15 04:04:20 -0700

    Update plotting

--- picca_analysis/plot_cf_colour.py
+++ picca_analysis/plot_cf_colour.py
@@ -17,19 +17,21 @@ plot_system = 'plot_per_file' #per bin or per file
 model = 'Slosar11'
 fit_type = 'manual'
 fit_data = {'b1': 1., 'b2': 1., 'beta1': 0., 'beta2': 0.}
-r_power = 2
-v_max = 10**-4
+r_power = 0
+vmax = 10**-4
 show_plots = True
 save_plots = True
 
+afix = 'fixed'
+rmin = 40.
 #Stuff we don't need for now.
 rmin_label = str(rmin)
 rmin_label = rmin_label[:rmin_label.find('.')]
-suffix = ''
-res_name = None#'/result'+suffix+'.h5'
+suffix = '_{}r_a{}'.format(rmin_label,afix)
+res_name = '/result'+suffix+'.h5'
 
 #Get the correlation objects
 corr_objects = plot_functions.get_correlation_objects(locations,res_name=res_name)
 
 #Make plots of the objects
-plot_functions.make_colour_plots(corr_objects,r_power=r_power,v_max=v_max,save_plots=True,show_plots=True,suffix=suffix)
+plot_functions.make_colour_plots(corr_objects,r_power=r_power,vmax=vmax,save_plots=True,show_plots=True,suffix=suffix)
