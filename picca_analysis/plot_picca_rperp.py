2b381ab49d6b82e22f8c69c02fa87b6f53fd0d54
James Farr <jfarr@cori09.nersc.gov>
2019-05-07 03:47:16 -0700

    Improved plotting  of picca

--- picca_analysis/plot_picca_rperp.py
+++ picca_analysis/plot_picca_rperp.py
@@ -16,7 +16,7 @@ else:
 #Plotting decisions
 plot_system = 'plot_per_file' #per bin or per file
 #Gaussian
-#fit_data = {'b1': 1.0, 'b2': 1.0, 'beta1': 0., 'beta2': 0.}
+fit_data = {'b1': 1.0, 'b2': 1.0, 'beta1': 0., 'beta2': 0.}
 #Lognormal
 #fit_data = {'b1': 1.0, 'b2': 1.0, 'beta1': 0., 'beta2': 0.}
 #Tau noRSD
@@ -25,7 +25,7 @@ plot_system = 'plot_per_file' #per bin or per file
 #fit_data = {'b1': 1.65*1.13, 'b2': 1.65*1.13, 'beta1': 0.9625/(1.65*1.13), 'beta2': 0.9625/(1.65*1.13)}
 #fit_data = {'b1': 2.22, 'b2': 2.22, 'beta1': 0.56, 'beta2': 0.56}
 #Flux
-fit_data = {'b1': -0.731, 'b2': -0.731, 'beta1': 0.514, 'beta2': 0.514}
+#fit_data = {'b1': -0.135, 'b2': -0.135, 'beta1': 1.178, 'beta2': 1.178}
 
 np_bins = 40
 bin_list = [0,5,10,15,20]
