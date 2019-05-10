fc5c0eaca9f17905801f64753290f3ccc8e51911
James Farr <james.a.farr@btopenworld.com>
2019-05-11 00:31:56 +0100

    reverted to old setup file

--- setup.py
+++ setup.py
@@ -1,25 +1,11 @@
-#!/usr/bin/env python
+from distutils.core import setup
 
-import glob
-
-from setuptools import setup
-
-scripts = glob.glob('bin/*')
-
-description = "Fast generation of Lya catalogs"
-
-version='0.1'
-setup(name="LyaCoLoRe",
-    version=version,
-    description=description,
-    url="https://github.com/igmhub/LyaCoLoRe",
-    author="<your name here>",
-    author_email="<your email here>",
-    packages=['LyaCoLoRe'],
-    package_dir = {'': 'py'},
-    package_data = {'LyaCoLoRe': ['etc/']},
-    install_requires=['numpy','scipy','iminuit','healpy','fitsio',
-        'numba','future','setuptools'],
-    test_suite='LyaCoLoRe.test.test_cor',
-    scripts = scripts
-    )
+setup(name='LyaCoLoRe',
+      version='0.1',
+      description='Fast generation of Lya catalogs',
+      author='igmhub',
+      author_email='github.com/igmhub',
+      url='https://github.com/igmhub/LyaCoLoRe',
+      packages=['lyacolore'],
+      package_dir = {'lyacolore': 'py'},
+     )
