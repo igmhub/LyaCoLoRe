fbd10c41272847641f1704ae19230b40cc8be6c5
James Farr <jfarr@cori12.nersc.gov>
2019-05-28 03:46:00 -0700

    Minor updates

--- scripts/measure_dla_distribution.py
+++ scripts/measure_dla_distribution.py
@@ -15,7 +15,7 @@ use_pyigm = True
 include_RSDs = True
 basedir = '/global/cscratch1/sd/jfarr/LyaSkewers/CoLoRe_GAUSS/test_improve_dlas/'
 #basedir = '/global/cscratch1/sd/jfarr/LyaSkewers/CoLoRe_GAUSS/v7/v7.0.0/'
-qso_z_buffer=0.2
+qso_z_buffer=0.05
 
 #Open a DLA master file and extract the relevant data.
 h = fits.open(basedir+'/master_DLA.fits')
@@ -166,7 +166,7 @@ plt.xlabel(r'$z$')
 plt.ylabel(r'$\mathrm{{d}}n / \mathrm{{d}}z$')
 plt.legend()
 plt.grid()
-plt.savefig('dndz_v7_improved_qsozbuffer{}.pdf'.format(qso_z_buffer))
+plt.savefig('dndz_v7_improved__qsozbuffer{}.pdf'.format(qso_z_buffer))
 plt.show()
 
 #Measure f(NHI)
@@ -182,24 +182,24 @@ for k,z_bin in enumerate(z_bins):
     #Measure the f values and scatter plot.
     z_val,log_NHI_values,f_values = measure_fNHI(dla_z,dla_NHI,z_bin,log_NHI_bin_edges,z_cell)
     non_empty = (f_values > 0)
-    #plt.scatter(log_NHI_values[non_empty],np.log10(f_values[non_empty]),label=r'$z={:1.2f}$'.format(z_val),c='C{}'.format(k),marker='x')
+    plt.scatter(log_NHI_values[non_empty],np.log10(f_values[non_empty]),label=r'$z={:1.2f}$'.format(z_val),c='C{}'.format(k),marker='x')
 
     #Compute the input from pyigm and plot a line,.
     log10_f_model = fN_default.evaluate(log_NHI_bin_edges,z_val).reshape(log_NHI_bin_edges.shape[0])
-    #plt.plot(log_NHI_bin_edges,log10_f_model,c='C{}'.format(k))
+    plt.plot(log_NHI_bin_edges,log10_f_model,c='C{}'.format(k))
 
     f_model_values = 10**fN_default.evaluate(log_NHI_values,z_val).reshape(log_NHI_values.shape[0])
-    plt.plot(log_NHI_values,(f_values-f_model_values)/(f_model_values),label=r'$z={:1.2f}$'.format(z_val),c='C{}'.format(k))
+    #plt.plot(log_NHI_values,(f_values-f_model_values)/(f_model_values),label=r'$z={:1.2f}$'.format(z_val),c='C{}'.format(k))
 
 #Control the axes etc of the plot.
 plt.xlim(17.0,23.0)
-#plt.ylim(-28.0,-17.0)
+plt.ylim(-28.0,-17.0)
 #plt.xlim(18.5,20.0)
 #plt.ylim(-21.0,-20.0)
-plt.ylim(-0.15,0.15)
-#plt.ylabel(r'$\log(f(N_{{HI}}))$')
+#plt.ylim(-0.15,0.15)
+plt.ylabel(r'$\log(f(N_{{HI}}))$')
 plt.xlabel(r'$\log(N_{{HI}})$')
 plt.legend()
 plt.grid()
-plt.savefig('f_NHI_v7_improved_residuals_qsozbuffer{}.pdf'.format(qso_z_buffer))
+plt.savefig('f_NHI_v7_improved_qsozbuffer{}.pdf'.format(qso_z_buffer))
 plt.show()
