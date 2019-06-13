c2ffc203889eadd3d5588b9b41c21eb3d701d471
jfarr03 <james.a.farr@btopenworld.com>
2019-06-13 11:34:21 -0700

    Updated formartting

--- scripts/show_lyacolore_stages.py
+++ scripts/show_lyacolore_stages.py
@@ -8,13 +8,14 @@ basedir = "/global/cscratch1/sd/jfarr/LyaSkewers/CoLoRe_GAUSS/v6/v6.0.0/"
 N_side = 16
 pixel = 0
 i_skewer = 0
+fontsize = 12
 
 #Plot variables.
 stages_1 = ['picca-gaussian', 'picca-density', 'picca-tau-notnorm', 'picca-flux-notnorm']
 label_1 = [r'$\delta_G$',r'1+$\delta$',r'$\tau$',r'$F$']
 stages_2 = ['picca-gaussian-colorecell', None, 'picca-tau-noRSD-notnorm', 'picca-flux-noRSD-notnorm']
 label_2 = [r'$\delta_C$',None,r'$\tau_\mathrm{noRSD}$',r'$F_\mathrm{noRSD}$']
-
+axis_label = ['Gaussian\nfield','Lognormal\ndensity','Optical\ndepth','Transmitted\nflux fraction']
 symmetrical = [True, False, False, False]
 add_one = [False,True,False,False]
 plot_types = ['skewer']
@@ -35,12 +36,13 @@ N_stages = len(stages_1)
 N_types = len(plot_types)
 
 #Make the subplots, and reduce the horizontal space between axes to 0.
-fig, axs = plt.subplots(N_stages, N_types, sharex=True, figsize=(8, 10), dpi= 80, facecolor='w', edgecolor='k')
-fig.subplots_adjust(hspace=0)
+plt.rc('xtick',labelsize=fontsize)
+plt.rc('ytick',labelsize=fontsize)
+fig, axs = plt.subplots(N_stages, N_types, sharex=True, figsize=(8, 8), dpi= 80, facecolor='w', edgecolor='k')
 
 for i in range(N_stages):
     dirname = utils.get_dir_name(basedir,pixel)
-    filename = utils.get_file_name(dirname,stages_1[i],N_side,pixel)[:-3]
+    filename = utils.get_file_name(dirname,stages_1[i],N_side,pixel)
     h = fits.open(filename)
     lambdas = 10**h[2].data
     skewer = h[0].data[:,i_skewer]
@@ -49,7 +51,7 @@ for i in range(N_stages):
     print(h[3].data['THING_ID'][i_skewer])
     axs[i].plot(lambdas,skewer,label=label_1[i],color=style_dict[stages_1[i]]['c'],linestyle=style_dict[stages_1[i]]['ls'])
     if stages_2[i]:
-        filename = utils.get_file_name(dirname,stages_2[i],N_side,pixel)[:-3]
+        filename = utils.get_file_name(dirname,stages_2[i],N_side,pixel)
         h_2 = fits.open(filename)
         lambdas_2 = 10**h_2[2].data
         skewer_2 = h_2[0].data[:,i_skewer]
@@ -70,13 +72,22 @@ for i in range(N_stages):
         y_upp_lim = np.max((abs(y_low_lim),abs(y_upp_lim)))
     axs[i].set_ylim(y_low_lim, y_upp_lim)
     axs[i].tick_params(which='both')
+    axs[i].set_ylabel(axis_label[i],rotation=90,fontsize=fontsize)
+
+    #Add a stage number/section reference:
+    axs[i].text(0, 0, 'Stage {}'.format(i), bbox={'facecolor': 'red', 'alpha': 0.5, 'pad': 4})
 
     #Add a grid and labels.
-    axs[i].grid()
-    axs[i].legend(loc=1,fontsize=20)
+    #axs[i].grid()
+    axs[i].legend(loc=1,fontsize=fontsize)
+    
+    #Add an arrow to show progression
+    axs[i].arrow(lambda_max*1.001, y_upp_lim, 0, y_low_lim-y_upp_lim, clip_on=False)
 
 plt.xlim(lambda_min,lambda_max)
-plt.xlabel(r'$\lambda\ /\ \mathrm{\AA}$',fontsize=20)
+plt.xlabel(r'$\lambda\ /\ \mathrm{\AA}$',fontsize=fontsize)
 plt.tight_layout()
+fig.subplots_adjust(hspace=0)
+#fig.align_ylabels()
 plt.savefig('skewers.pdf')
 plt.show()
