d8529cae0c7d3c460ba8f81976b8195dc29c775a
jfarr03 <james.a.farr@btopenworld.com>
2019-06-13 12:44:58 +0100

    Improvements and inclusion of xcf

--- run_picca_multi_node.sh
+++ run_picca_multi_node.sh
@@ -1,4 +1,8 @@
-### START OF OPTIONS ###
+############################### START OF OPTIONS ###############################
+
+## WHAT TO MEASURE ##
+CF=0
+XCF=1
 
 ## INPUT DATA OPTIONS ##
 #number of pixels
@@ -13,7 +17,7 @@ INDIR="/global/cscratch1/sd/jfarr/LyaSkewers/CoLoRe_GAUSS/v7/v7_full_lr1200_tune
 #QUANTITY2="gaussian"
 #INDIR2="/global/cscratch1/sd/jfarr/LyaSkewers/CoLoRe_GAUSS/v7/v7.0.0/"
 #INDIR2="/global/cscratch1/sd/jfarr/LyaSkewers/CoLoRe_GAUSS/test_velocity_interpolation_full_no_ssf/"
-DRQ="/global/cscratch1/sd/jfarr/LyaSkewers/CoLoRe_GAUSS/v7/v7_full_lr1200_tuned_vel1.3/master_picca_RSD.fits"
+DRQ="${INDIR}/master_picca_RSD.fits"
 NSIDELYACOLORE=16
 
 ## NERSC OPTIONS ##
@@ -34,13 +38,16 @@ NP=40
 NT=40
 ZMIN=2.4
 ZMAX=2.6
-CORRTYPE="cf"
+ZMINQSO=2.4
+ZMAXQSO=2.6
 NSIDEPICCA=16
 
-##O UTPUT OPTIONS ##
+## OUTPUT OPTIONS ##
 ANALYSIS_ID=40
-OUTPUT_FILENAME="cf.fits.gz"
-OUTPUT_EXP_FILENAME="cf_exp.fits.gz"
+CF_OUTPUT_FILENAME="cf.fits.gz"
+CF_OUTPUT_EXP_FILENAME="cf_exp.fits.gz"
+XCF_OUTPUT_FILENAME="xcf.fits.gz"
+XCF_OUTPUT_EXP_FILENAME="xcf_exp.fits.gz"
 
 ## FIT OPTIONS ##
 #Set the fitting bin properties
@@ -48,7 +55,7 @@ RMINS='20.0 40.0 60.0'
 RMAXS=160.0
 AFIXS='fixed free'
 
-### END OF OPTIONS ###
+################################ END OF OPTIONS ################################
 
 #format analysis number
 ANALYSIS_ID=`printf "%03d" ${ANALYSIS_ID}`
@@ -65,24 +72,23 @@ echo "output will written to "$OUTPUT_PATH
 if [ ! -d $OUTPUT_PATH ] ; then
     mkdir -p $OUTPUT_PATH
 fi
-NODE_OUTPUT_PATH="$OUTPUT_PATH/node_cfs/"
+NODE_OUTPUT_PATH="$OUTPUT_PATH/node_outputs/"
 mkdir $NODE_OUTPUT_PATH
-echo "output logs will be saved to "$OUTPUT_PATH"/logs"
 if [ ! -d $OUTPUT_PATH/logs ] ; then
     mkdir -p $OUTPUT_PATH/logs
 fi
 
 #make paths to output files
-OUTPUT_FILE="${OUTPUT_PATH}/${OUTPUT_FILENAME}"
-OUTPUT_EXP_FILE="${OUTPUT_PATH}/${OUTPUT_EXP_FILENAME}"
+CF_OUTPUT_FILE="${OUTPUT_PATH}/${CF_OUTPUT_FILENAME}"
+CF_OUTPUT_EXP_FILE="${OUTPUT_PATH}/${CF_OUTPUT_EXP_FILENAME}"
+XCF_OUTPUT_FILE="${OUTPUT_PATH}/${XCF_OUTPUT_FILENAME}"
+XCF_OUTPUT_EXP_FILE="${OUTPUT_PATH}/${XCF_OUTPUT_EXP_FILENAME}"
 
-# we will create this script
-RUN_FILE="${OUTPUT_PATH}/run_picca_cf_${ANALYSIS_ID}_${PICCA_ID}.sh"
-echo "run file "${RUN_FILE}
-
-date
-
-cat > $RUN_FILE <<EOF
+# create scripts
+if [ $CF -eq 1 ]; then
+CF_RUN_FILE="${OUTPUT_PATH}/run_picca_cf_${ANALYSIS_ID}_${PICCA_ID}.sh"
+echo "cf run file "${RUN_FILE}
+cat > $CF_RUN_FILE <<EOF
 #!/bin/bash -l
 
 #SBATCH --partition ${QUEUE}
@@ -195,6 +201,118 @@ date
 /global/homes/j/jfarr/Projects/LyaCoLoRe/picca_analysis/make_picca_aux_files.py --base-dir ${OUTPUT_PATH} --corr-type $CORRTYPE --quantity $QUANTITY --npixels ${NPIXELS} --quant-code ${QC} --zmin ${ZMIN} --zmax ${ZMAX} --cf-filename $OUTPUT_FILENAME --cf-exp-filename $OUTPUT_EXP_FILENAME --nside $NSIDEPICCA --rpmin ${RPMIN} --rpmax ${RPMAX} --rtmin ${RTMIN} --rtmax ${RTMAX} --np ${NP} --nt ${NT} --rmin-values ${RMINS} --rmax-values ${RMAXS} --afix-values ${AFIXS}
 
 EOF
+fi
 
-# send the job to the queue
-sbatch $RUN_FILE
+if [ $XCF -eq 1 ]; then
+XCF_RUN_FILE="${OUTPUT_PATH}/run_picca_xcf_${ANALYSIS_ID}_${PICCA_ID}.sh"
+echo "xcf run file "${RUN_FILE}
+cat > $XCF_RUN_FILE <<EOF
+#!/bin/bash -l
+
+#SBATCH --partition ${QUEUE}
+#SBATCH --nodes ${NNODES}
+#SBATCH --time ${TIME}
+#SBATCH --job-name picca-${CORRTYPE}-${QC}-${NPIXELS}
+#SBATCH --error "${OUTPUT_PATH}/picca-xcf-%j.err"
+#SBATCH --output "${OUTPUT_PATH}/picca-xcf-%j.out"
+#SBATCH -C haswell
+#SBATCH -A desi
+
+umask 0002
+export OMP_NUM_THREADS=64
+
+if [ $(( $NPIXPERNODE * $NNODES )) -gt $NPIXELS ]; then
+    echo "$(( $NPIXPERNODE * $NNODES )) pixels will be used (rather than ${NPIXELS})."
+    echo "This ensures that each of the $NNODES nodes gets the same number of pixels: $NPIXPERNODE."
+fi
+
+INDIR="$INDIR"
+INDIR2="$INDIR2"
+START_INDEX=0
+STOP_INDEX=$(( $NPIXPERNODE - 1 ))
+FINAL_NODE=0
+N_TOTAL=0
+N_REMAINING=${NPIXELS}
+i=0
+
+for NODE in \`seq $NNODES\` ; do
+    echo "starting node \$NODE"
+
+    N=0
+    NODE_FILES=""
+    NODE_FILES2=""
+    while [ \${N} -lt $NPIXPERNODE ]; do
+
+        F="${INDIR}/\$(( \${i}/100 ))/\${i}/picca-${QUANTITY}-16-\${i}.fits.gz";
+
+        if [ -z "\${INDIR2}" ]; then
+
+            if [ -f "\$F" ]; then
+                NODE_FILES="\$NODE_FILES \$F";
+                N=\$(( \$N + 1 ));
+                N_TOTAL=\$(( \$N_TOTAL + 1 ));
+                N_REMAINING=\$(( \$N_REMAINING - 1 ));
+            fi;
+
+        else
+
+            F2="${INDIR2}/\$(( \${i}/100 ))/\${i}/picca-${QUANTITY2}-16-\${i}.fits.gz";
+
+            if [ -f "\$F" ] && [ -f "\$F2" ]; then
+                NODE_FILES="\$NODE_FILES \$F";
+                NODE_FILES2="\$NODE_FILES2 \$F2";
+                N=\$(( \$N + 1 ));
+                N_TOTAL=\$(( \$N_TOTAL + 1 ));
+                N_REMAINING=\$(( \$N_REMAINING - 1 ));
+            fi;
+
+        fi;
+
+        i=\$(( \$i + 1 ));
+
+        if [ \$i -gt $(( 12 * $NSIDELYACOLORE * $NSIDELYACOLORE )) ]; then
+            N=$NPIXPERNODE;
+            echo "not enough files found for node \$NODE: moving on...";
+        fi;
+
+    done
+
+    NODE_OUTPUT_FILE="${NODE_OUTPUT_PATH}/xcf_\${START_INDEX}_\${STOP_INDEX}.fits.gz"
+
+    command="srun -N 1 -n 1 -c ${NCORES} ${PICCA_PATH}/bin/picca_xcf.py --in-dir \"dummy\" --from-image \${NODE_FILES} --drq ${DRQ} --out \${NODE_OUTPUT_FILE} --rp-min ${RPMIN} --rp-max ${RPMAX} --rt-max ${RTMAX} --np ${NP} --nt ${NT} --no-project --nside ${NSIDEPICCA} --nproc 64 --z-cut-min ${ZMIN} --z-cut-max ${ZMAX} --z-min-obj ${ZMINQSO} --z-max-obj ${ZMAXQSO}";
+
+    echo \$command
+    \$command >& ${OUTPUT_PATH}/logs/node-\${NODE}.log &
+
+    START_INDEX=\$(( \$STOP_INDEX + 1 ))
+    STOP_INDEX=\$(( \$START_INDEX + $NPIXPERNODE - 1))
+
+    if (( \$FINAL_NODE == 1)) ; then
+        echo "all files allocated, no more nodes needed"
+        break
+    fi
+
+    if (( \${N_TOTAL} >= $(( $NPIXELS - 1)) )) ; then
+        FINAL_NODE=1
+    fi
+
+done
+
+wait
+
+#Combine all of the individual output files together
+/global/homes/j/jfarr/Projects/LyaCoLoRe/picca_analysis/combine_picca_cf_files.py --in-dir ${NODE_OUTPUT_PATH} --out ${OUTPUT_FILE}
+
+#Export
+${PICCA_PATH}/bin/picca_export.py --data ${OUTPUT_FILE} --out ${OUTPUT_EXP_FILE}
+date
+
+#Generate the aux files
+/global/homes/j/jfarr/Projects/LyaCoLoRe/picca_analysis/make_picca_aux_files.py --base-dir ${OUTPUT_PATH} --corr-type $CORRTYPE --quantity $QUANTITY --npixels ${NPIXELS} --quant-code ${QC} --zmin ${ZMIN} --zmax ${ZMAX} --cf-filename $OUTPUT_FILENAME --cf-exp-filename $OUTPUT_EXP_FILENAME --nside $NSIDEPICCA --rpmin ${RPMIN} --rpmax ${RPMAX} --rtmin ${RTMIN} --rtmax ${RTMAX} --np ${NP} --nt ${NT} --rmin-values ${RMINS} --rmax-values ${RMAXS} --afix-values ${AFIXS}
+
+EOF
+fi
+
+# send the jobs to the queue
+sbatch $CF_RUN_FILE
+sbatch $XCF_RUN_FILE
