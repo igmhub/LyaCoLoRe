edce351b2e1a4259b8dce180d13e64de80c42ea1
jfarr03 <james.a.farr@btopenworld.com>
2019-06-20 11:36:31 -0700

    Updated to latest results

--- picca_analysis/plot_picca.py
+++ picca_analysis/plot_picca.py
@@ -14,14 +14,15 @@ figsize = (12, 5)
 dpi = 80
 show_plot = True
 save_plot = True
-filename = 'corr_plot.pdf'
+filename = 'corr_plot_systematics.pdf'
 
 #Create a dictionary with all information about the subplots:
-# TODO: Implement a way to look up the bias and betas rather than by hand?
-#'/global/homes/j/jfarr/Programs/picca/picca_analysis_042/picca_00211/'
+
+#Main method correlations plot:
+filename = 'corr_plot.pdf'
 subplots = {}
-subplots[(0,0)] =  {'location':         '/Users/jfarr/Downloads/picca_00215/',
-                    'filename':         'xcf_exp.fits.gz',
+subplots[(0,0)] =  {'location':         '/global/homes/j/jfarr/Programs/picca/picca_analysis_037/combined/',
+                    'filename':         'cf_exp.fits.gz',
                     'mu_bins':          [(0.0,0.5),(0.5,0.8),(0.8,0.95),(0.95,1.0)],
                     'mu_bin_colours':   ['C0','C1','C2','C3'],
                     'plot_data':        {'r_power': 2, 'nr': 40, 'rmax': 160.0},
@@ -31,17 +32,46 @@ subplots[(0,0)] =  {'location':         '/Users/jfarr/Downloads/picca_00215/',
                     'manual_fit_data':  {'b1': -0.133, 'b2': -0.133, 'beta1': 1.4, 'beta2': 1.4},
                     'format':           {'legend': True, 'xlabel': True, 'ylabel': True},
                     }
-subplots[(0,1)] =  {'location':         '/Users/jfarr/Downloads/picca_00215/',
-                    'filename':         'xcf_exp.fits.gz',
+subplots[(0,1)] =  {'location':         '/global/homes/j/jfarr/Programs/picca/picca_analysis_041/picca_00223/',
+                    'filename':         'xcf_exp_2400k_shuffle.fits.gz',
                     'mu_bins':          [(0.0,0.5),(0.5,0.8),(0.8,0.95),(0.95,1.0)],
                     'mu_bin_colours':   ['C0','C1','C2','C3'],
                     'plot_data':        {'r_power': 2, 'nr': 40, 'rmax': 160.0},
                     'plot_picca_fit':   True,
                     'picca_fit_data':   {'rmin': 40., 'afix': 'free'},
                     'plot_manual_fit':  False,
-                    'manual_fit_data':  {'b1': -0.133, 'b2': -0.133, 'beta1': 1.4, 'beta2': 1.4},
+                    'manual_fit_data':  {'b1': -0.133, 'b2': 2.0, 'beta1': 1.4, 'beta2': 0.79},
                     'format':           {'legend': False, 'xlabel': True, 'ylabel': True},
                     }
+
+"""
+
+#Systematics correlations plot:
+filename = 'corr_plot_systematics.pdf'
+subplots = {}
+subplots[(0,0)] =  {'location':         '/global/homes/j/jfarr/Programs/picca/picca_analysis_042/picca_00211/',
+                    'filename':         'cf_exp.fits.gz',
+                    'mu_bins':          [(0.0,0.5),(0.5,0.8),(0.8,0.95),(0.95,1.0)],
+                    'mu_bin_colours':   ['C0','C1','C2','C3'],
+                    'plot_data':        {'r_power': 2, 'nr': 40, 'rmax': 160.0},
+                    'plot_picca_fit':   False,
+                    'picca_fit_data':   {'rmin': 40., 'afix': 'free'},
+                    'plot_manual_fit':  True,
+                    'manual_fit_data':  {'b1': -0.133, 'b2': -0.133, 'beta1': 1.4, 'beta2': 1.4},
+                    'format':           {'legend': True, 'xlabel': True, 'ylabel': True},
+                    }
+subplots[(0,1)] =  {'location':         '/global/homes/j/jfarr/Programs/picca/picca_analysis_041/picca_00214/',
+                    'filename':         'xcf_exp_noshuffle.fits.gz',
+                    'mu_bins':          [(0.0,0.5),(0.5,0.8),(0.8,0.95),(0.95,1.0)],
+                    'mu_bin_colours':   ['C0','C1','C2','C3'],
+                    'plot_data':        {'r_power': 2, 'nr': 40, 'rmax': 160.0},
+                    'plot_picca_fit':   False,
+                    'picca_fit_data':   {'rmin': 40., 'afix': 'free'},
+                    'plot_manual_fit':  True,
+                    'manual_fit_data':  {'b1': -0.133, 'b2': 2.0, 'beta1': 1.4, 'beta2': 0.79},
+                    'format':           {'legend': False, 'xlabel': True, 'ylabel': True},
+                    }
+"""
 
 ################################################################################
 
