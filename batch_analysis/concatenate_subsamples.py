e9759be2e269b3ad3326191aafb96dee9f665033
jfarr03 <james.a.farr@btopenworld.com>
2019-09-20 02:26:56 -0700

    Bug fixes

--- batch_analysis/concatenate_subsamples.py
+++ batch_analysis/concatenate_subsamples.py
@@ -9,16 +9,19 @@ for ct in corr_types:
     #Set up the data structures to store the information.
     cor_data = []
     cor = {}
-    h = fits.open('/project/projectdirs/desi/users/jfarr/LyaCoLoRe_paper/analysis/correlation_functions/{}/measurements/{}/correlations/cf_{}.fits.gz'.format(vers[0],ct,ct))
+    ver = 'v9.0.{}'.format(realisations[0])
+    h = fits.open('/project/projectdirs/desi/users/jfarr/LyaCoLoRe_paper/analysis/correlation_functions/{}/measurements/{}/correlations/cf_{}.fits.gz'.format(ver,ct,ct))
     attri = {}
-    for k in ['RP','RT','Z','NB']
-        data[k] = np.zeros(h[1].data[k].shape)
+    for k in ['RP','RT','Z','NB']:
+        attri[k] = np.zeros(h[1].data[k].shape)
     attri_header = fits.Header()
     for k in ['RPMIN','RPMAX','RTMAX','NP','NT','ZCUTMIN','ZCUTMAX','NSIDE']:
-        cor_header[k] = h[1].header[k]
+        attri_header[k] = h[1].header[k]
     cor_header = fits.Header()
     for k in ['HLPXSCHM']:
         cor_header[k] = h[2].header[k]
+    attri_dtype = h[1].data.dtype
+    cor_dtype = h[2].data.dtype
     h.close()
 
     #Loop through the files, adding the data to our overarching structures.
@@ -28,7 +31,7 @@ for ct in corr_types:
         for k in ['RP','RT','Z']:
             attri[k] += h[1].data[k]*h[1].data['NB']
         attri['NB'] += h[1].data['NB']
-        cor += [h[2].data]
+        cor_data += [h[2].data]
         h.close()
 
     #Ensure that data are correctly normalised.
@@ -36,12 +39,15 @@ for ct in corr_types:
         attri[k] /= attri['NB']
 
     for k in ['WE','DA']:
-        cor[k] = np.concatenate([cd[k] for cd in cor_datas])
+        cor[k] = np.concatenate([cd[k] for cd in cor_data],axis=0)
 
-    cor['HEALPID'] = np.concatenate([cd['HEALPID']+realisations[i]*(10**5) for i,cd in enumerate(cor_data)])
+    cor['HEALPID'] = np.concatenate([cd['HEALPID']+realisations[i]*(10**5) for i,cd in enumerate(cor_data)],axis=0)
 
-    attri_table = np.array([attri[k]] for k in ['RP','RT','Z','NB'],dtype = ['RP','RT','Z','NB'])
-    cor_table = np.array([attri[k]] for k in ['WE','DA','HEALPID'],dtype = ['WE','DA','HEALPID'])
+    attri_table = np.array([attri[k] for k in attri_dtype.names],dtype=attri_dtype)
+    cor_table = np.array([cor[k] for k in cor_dtype.names],dtype=cor_dtype)
+
+    print(attri_table.shape)
+    print(cor_table.shape)
 
     prihdr = fits.Header()
     prihdu = fits.PrimaryHDU(header=prihdr)
