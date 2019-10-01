953b06f0ce9eeea61710762e48ea7561f56f8617
jfarr03 <james.a.farr@btopenworld.com>
2019-10-01 07:49:47 -0700

    Minor changes

--- scripts/show_lyacolore_stages.py
+++ scripts/show_lyacolore_stages.py
@@ -1,15 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from astropy.io import fits
+from scipy.interpolate import interp1d
 
 from lyacolore import utils
 
-basedir = "/global/cscratch1/sd/jfarr/LyaSkewers/CoLoRe_GAUSS/v6/v6.0.0/"
+lya = utils.lya_rest
+
+basedir = "/global/cscratch1/sd/jfarr/LyaSkewers/CoLoRe_GAUSS/v9/v9.0.0_all_files/"
 N_side = 16
 pixel = 0
-i_skewer = 0
+i_skewer = 11
 fontsize = 16
-plotsize = (12,15)
+figsize = (12,15)
 dpi = 80
 
 #Set style options everywhere.
@@ -19,15 +22,15 @@ plt.rc('font', size=fontsize)
 #Plot variables.
 stages_1 = ['picca-gaussian', 'picca-density', 'picca-tau-notnorm', 'picca-flux-notnorm']
 label_1 =  [r'$\delta_G$',r'1+$\delta$',r'$\tau$',r'$F$']
-stages_2 = ['picca-gaussian-colorecell', None, 'picca-tau-noRSD-notnorm', 'picca-flux-noRSD-notnorm']
-label_2 =  [r'$\delta_C$',None,r'$\tau_\mathrm{noRSD}$',r'$F_\mathrm{noRSD}$']
+stages_2 = ['picca-gaussian-colorecell', None, 'picca-tau-noRSD-notnorm', None]
+label_2 =  [r'$\delta_C$',None,r'$\tau_\mathrm{noRSD}$',None]
 axis_label = ['Gaussian\nfield','Lognormal\ndensity','Optical\ndepth','Transmitted\nflux fraction']
 symmetrical = [True, False, False, False]
 add_one = [False,True,False,False]
 h_lines = {0: [0], 1: [0], 2: [0], 3: [0,1]}
 plot_types = ['skewer']
-lambda_min = 3750. #Angstroms
-lambda_max = 3850. #Angstroms
+lambda_min = 3800. #Angstroms
+lambda_max = 3900. #Angstroms
 
 style_dict = {'picca-gaussian-colorecell': {'c': 'C0', 'ls': '--'},
               'picca-gaussian':            {'c': 'C0', 'ls': '-'},
@@ -47,21 +50,33 @@ fig, axs = plt.subplots(N_stages, N_types, sharex=True, figsize=figsize, dpi=dpi
 
 for i in range(N_stages):
     dirname = utils.get_dir_name(basedir,pixel)
-    filename = utils.get_file_name(dirname,stages_1[i],N_side,pixel)
+    filename = utils.get_file_name(dirname,stages_1[i],N_side,pixel,compressed=True)
     h = fits.open(filename)
+    mockid_1 = h[3].data['THING_ID'][i_skewer]
     lambdas = 10**h[2].data
     skewer = h[0].data[:,i_skewer]
     if add_one[i]:
         skewer += 1.
-    print(h[3].data['THING_ID'][i_skewer])
+    print(mockid_1)
     axs[i].plot(lambdas,skewer,label=label_1[i],color=style_dict[stages_1[i]]['c'],linestyle=style_dict[stages_1[i]]['ls'])
     if stages_2[i]:
-        filename = utils.get_file_name(dirname,stages_2[i],N_side,pixel)
+        filename = utils.get_file_name(dirname,stages_2[i],N_side,pixel,compressed=True)
         h_2 = fits.open(filename)
+        mockid_2 = h[3].data['THING_ID'][i_skewer]
         lambdas_2 = 10**h_2[2].data
         skewer_2 = h_2[0].data[:,i_skewer]
         axs[i].plot(lambdas_2,skewer_2,label=label_2[i],color=style_dict[stages_2[i]]['c'],linestyle=style_dict[stages_2[i]]['ls'])
-        print(h_2[3].data['THING_ID'][i_skewer])
+        print(mockid_2)
+
+        if 'RSD' in stages_2[i]:
+            filename = utils.get_file_name(dirname,'gaussian-colore',N_side,pixel,compressed=True)
+            h = fits.open(filename)
+            lambdas_vel = lya*(1+h[4].data['Z'][:])
+            i_col = np.where(h[1].data['MOCKID']==mockid_2)[0][0]
+            print(i_col)
+            vel = h[3].data[i_col,:]
+            axs[i].plot(lambdas_vel,vel*5000,label='vel*5000',color='grey',linestyle=style_dict[stages_2[i]]['ls'])
+
     print(' ')
 
     #Scale the axes nicely given the x axis limits.
@@ -97,9 +112,36 @@ for i in range(N_stages):
         axs[i].axhline(y=h_val,color='gray',zorder=0,alpha=0.5)
 
 plt.xlim(lambda_min,lambda_max)
-plt.xlabel(r'$\lambda\ /\ \mathrm{\AA}$')
-plt.tight_layout()
-fig.subplots_adjust(hspace=0)
+plt.xlabel(r'$\lambda\ [\mathrm{\AA}]$')
 #fig.align_ylabels()
+
+m = fits.open(basedir+'/master.fits')
+l = (1+m['COSMO_EXP'].data['Z'])*utils.lya_rest
+r = m['COSMO_EXP'].data['R']
+r_to_l = interp1d(l,r)
+l_to_r = interp1d(r,l)
+topax = axs[0].twiny()
+
+print(l[:5])
+print(r[:5])
+print(' ')
+print(l[5:10])
+print(r[5:10])
+print(' ')
+
+topax_values = [3940,3980,4020,4060]
+topax_locations = r_to_l(topax_values)
+
+#topax_locations = axs[0].get_xticks()
+#topax_values = l_to_r(topax_locations)
+
+topax_strs = [str(round(xval,0)) for xval in topax_values]
+topax.set_xlim(axs[0].get_xlim())
+topax.set_xticks(topax_locations)
+topax.set_xticklabels(topax_strs)
+topax.set_xlabel(r'$r\ [\rm{Mpc}/h]$')
+
+fig.tight_layout()
+fig.subplots_adjust(hspace=0)
 plt.savefig('skewers.pdf')
 plt.show()
