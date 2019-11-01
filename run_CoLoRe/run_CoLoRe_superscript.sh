07dc7cbc01eb67807424735cc470355a3dd517eb
jfarr03 <james.a.farr@btopenworld.com>
2019-11-01 09:51:32 -0700

    Bug fix

--- run_CoLoRe/run_CoLoRe_superscript.sh
+++ run_CoLoRe/run_CoLoRe_superscript.sh
@@ -9,9 +9,12 @@ SEEDS=$(seq $START_SEED $((START_SEED + NBOXES - 1)))
 #<5 boxes can use debug, otherwise regular
 QUEUE='debug'
 
-# specify where to output the box to
-OUTDIR="/global/cscratch1/sd/jfarr/LyaSkewers/CoLoRe_GAUSS/CoLoRe_seed${SEED}/"
-
 for SEED in $SEEDS; do
+
+# specify where to output the box to
+OUTDIR="/global/cscratch1/sd/jfarr/LyaSkewers/CoLoRe_GAUSS/CoLoRe_seed${SEED}_4096/"
+
+# run colore
 source run_CoLoRe.sh ${SEED} ${QUEUE} ${OUTDIR};
+
 done
