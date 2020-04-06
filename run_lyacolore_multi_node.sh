00dd65918612a8bb7f107113bd2c73fdfb1a18e1
jfarr03 <james.a.farr@btopenworld.com>
2020-04-06 08:18:41 -0700

    Randomised ordering of pixels + bug fixes

--- run_lyacolore_multi_node.sh
+++ run_lyacolore_multi_node.sh
@@ -14,14 +14,14 @@ CONFIG_FILE="./input_files/config_files/config_v9.0.ini"
 
 # Set where your CoLoRe output is located, and where you would like your
 # LyaCoLoRe output to be located.
-COLORE_OUT_LOC=...
-LYACOLORE_OUT_LOC=...
+COLORE_OUT_LOC="..."
+LYACOLORE_OUT_LOC="..."
 
 # Specify the queue to use for this set of realisations, and the number of
 # nodes/cores.
 QUEUE='debug'
 NNODES=32
-NCORES=32
+NCORES=64
 TIME="00:10:00" #hh:mm:ss
 
 # Specify the settings for LyaCoLoRe.
@@ -35,10 +35,9 @@ echo " "
 echo "################################################################################"
 echo " "
 echo "CoLoRe input will be taken from "$COLORE_OUT_LOC
-INPUT_FILES=`ls -1 ${INPUT_PATH}/out_srcs_*.fits`
+INPUT_FILES=`ls -1 ${COLORE_OUT_LOC}/out_srcs_*.fits`
 NFILES=`echo $INPUT_FILES | wc -w`
 echo " -> ${NFILES} input files have been found"
-echo "Using LyaCoLoRe seed $LYACOLORE_SEED"
 echo "Output will written to "$LYACOLORE_OUT_LOC
 if [ ! -d $LYACOLORE_OUT_LOC ] ; then
     mkdir -p $LYACOLORE_OUT_LOC
@@ -75,10 +74,11 @@ cat > $RUN_FILE <<EOF
 #SBATCH -C haswell
 #SBATCH -A desi
 
+conda activate lyacolore
 umask 0002
 export OMP_NUM_THREADS=64
 
-PIXDIRS=\`\ls -tr1d ${LYACOLORE_OUT_LOC}/[0-9]*/*\`
+PIXDIRS=\`\ls -tr1d ${LYACOLORE_OUT_LOC}/[0-9]*/* | sort -R\`
 NPIXELS=\`echo \$PIXDIRS | wc -w\`
 PIXDIRS_list=(\$PIXDIRS)
 
@@ -100,7 +100,7 @@ for NODE in \`seq $NNODES\` ; do
 
     echo "looking at pixels: \${NODE_PIXELS}"
 
-    command="${LYACOLORE_PATH}/make_transmission.py -c ${CONFIG_FILE} -i ${COLORE_OUT_LOC} -o ${LYACOLORE_OUT_LOC} --nproc ${NCORES}"
+    command="${LYACOLORE_PATH}/scripts/make_transmission.py -c ${CONFIG_FILE} -i ${COLORE_OUT_LOC} -o ${LYACOLORE_OUT_LOC} --nproc ${NCORES} --pixels \${NODE_PIXELS}"
 
     echo \$command
     \$command >& ${LYACOLORE_OUT_LOC}/logs/node-\${NODE}.log &
