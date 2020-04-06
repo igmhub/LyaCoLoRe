da122ea7ac4aee242a6eb923ad9f312a49fd56b4
jfarr03 <james.a.farr@btopenworld.com>
2020-04-06 05:33:29 -0700

    Bug fix

--- run_lyacolore.sh
+++ run_lyacolore.sh
@@ -10,7 +10,7 @@
 # Set the config file that we want to point to. Look at the config files to get
 # some more detail on the arguments chosen, and  for instructions on how to find
 # out more about the options.
-CONFIG_FILE=./input_files/config_files/config_v9.0.ini
+CONFIG_FILE="./input_files/config_files/config_v9.0.ini"
 
 # Set where your CoLoRe output is located, and where you would like your
 # LyaCoLoRe output to be located.
@@ -18,7 +18,7 @@ COLORE_OUT_LOC=...
 LYACOLORE_OUT_LOC=...
 
 # Specify number of cores to use.
-NCORES=32
+NCORES=64
 
 ## END OF USER DEFINED PARAMS.
 ################################################################################
@@ -37,7 +37,7 @@ $command
 echo " "
 echo " 2. Make transmission files"
 echo " "
-command="${LYACOLORE_PATH}/scripts/make_transmission.py -c ${CONFIG_FILE} --nproc ${NCORES}"
+command="${LYACOLORE_PATH}/scripts/make_transmission.py -c ${CONFIG_FILE} -i ${COLORE_OUT_LOC} -o ${LYACOLORE_OUT_LOC} --nproc ${NCORES}"
 $command
 
 echo " "
