ee7f239030000cf792a447f72329a7ceb64b842d
jfarr03 <james.a.farr@btopenworld.com>
2020-10-23 22:51:15 +0100

    Update

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 import glob
 
 scripts = glob.glob('scripts/*')
@@ -7,10 +7,12 @@ setup(name='LyaCoLoRe',
       version='0.1',
       description='Fast generation of Lya catalogs',
       author='igmhub',
-      author_email='github.com/igmhub',
+      author_email='james.farr.17@ucl.ac.uk',
       url='https://github.com/igmhub/LyaCoLoRe',
       packages=['lyacolore'],
       package_dir = {'lyacolore': 'py'},
+      install_requires=['numpy','scipy','iminuit','healpy','fitsio',
+          'numba','future','setuptools','configargparse'],
       test_suite='picca.test.test_cor',
       scripts = scripts
      )
