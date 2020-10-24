829c9b265f7cff945cec843078cc71e004d065b9
jfarr03 <james.a.farr@btopenworld.com>
2020-10-24 14:17:15 +0100

    Made changes from update_travis manually

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
-from distutils.core import setup
+#!/usr/bin/env python
+
+from setuptools import setup
+import glob
+
+scripts = glob.glob('scripts/*')
 
 setup(name='LyaCoLoRe',
       version='0.1',
       description='Fast generation of Lya catalogs',
       author='igmhub',
-      author_email='github.com/igmhub',
+      author_email='james.farr.17@ucl.ac.uk',
       url='https://github.com/igmhub/LyaCoLoRe',
       packages=['lyacolore'],
-      package_dir = {'lyacolore': 'py'},
+      package_dir = {'':'py', 'lyacolore': 'py/lyacolore'},
+      install_requires=['numpy','scipy','iminuit','healpy','fitsio',
+          'numba','future','setuptools','configargparse'],
+      test_suite='lyacolore.test.test_cor',
+      scripts = scripts
      )
