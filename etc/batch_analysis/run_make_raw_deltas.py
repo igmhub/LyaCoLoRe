4d8f7fefa541503243cc2241021da6fe00c71203
jfarr03 <james.a.farr@btopenworld.com>
2020-11-23 22:49:47 +0000

    Updated job name for clarify

--- etc/batch_analysis/run_make_raw_deltas.py
+++ etc/batch_analysis/run_make_raw_deltas.py
@@ -98,7 +98,7 @@ with open(args.python_script,'w') as f:
 
 ## Make the slurm script text.
 time = submit_utils.nh_to_hhmmss(args.slurm_hours)
-text = submit_utils.make_header(queue='regular',nnodes=1,time=time,job_name='picca_deltas',err_file='run-picca-deltas-%j.err',out_file='run-picca-deltas-%j.out')
+text = submit_utils.make_header(queue='regular',nnodes=1,time=time,job_name='raw_picca_deltas',err_file='run-picca-deltas-%j.err',out_file='run-picca-deltas-%j.out')
 text += 'export OMP_NUM_THREADS=1\n'
 text += 'srun -n 1 -c 64 {}\n'.format(args.python_script)
 
