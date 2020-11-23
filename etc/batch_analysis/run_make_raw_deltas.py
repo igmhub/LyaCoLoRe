21c0ca2315efe4fd489303da2c00f4c78b5f874c
jfarr03 <james.a.farr@btopenworld.com>
2020-11-23 23:08:59 +0000

    Fixes

--- etc/batch_analysis/run_make_raw_deltas.py
+++ etc/batch_analysis/run_make_raw_deltas.py
@@ -104,7 +104,9 @@ with open(args.python_script,'w') as f:
 
 ## Make the slurm script text.
 time = submit_utils.nh_to_hhmmss(args.slurm_hours)
-text = submit_utils.make_header(queue=args.slurm_queue,nnodes=1,time=time,job_name='raw_picca_deltas',err_file='run-picca-deltas-%j.err',out_file='run-picca-deltas-%j.out')
+err_file = os.path.join(args.out_dir,'run-picca-deltas-%j.err')
+out_file = os.path.join(args.out_dir,'run-picca-deltas-%j.out')
+text = submit_utils.make_header(queue=args.slurm_queue,nnodes=1,time=time,job_name='raw_picca_deltas',err_file=err_file,out_file=out_file)
 text += 'export OMP_NUM_THREADS=1\n'
 text += 'srun -n 1 -c 64 {}\n'.format(args.python_script)
 
